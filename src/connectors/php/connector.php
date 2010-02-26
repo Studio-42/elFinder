0027713c8f3b65d54e6d67f8789637f01a50d095
Dmitry Levashov <dio@std42.ru>
2010-02-27 02:56:09 +0300

    add setlocale to utf8 in php to correct multibyte chracters

--- src/connectors/php/connector.php
+++ src/connectors/php/connector.php
@@ -30,6 +30,9 @@ $opts = array(
 	'root'   => '../../../files',         // path to root directory
 	'URL'    => 'http://localhost/git/elfinder/files/', // root directory URL
 	'rootAlias'    => 'Home',          // display this instead of root directory name
+	'uploadAllow' => array("jpg","jpeg","gif","png","zip"),
+	'uploadDeny' => array('all'),
+	'uploadOrder' => 'deny,allow'
 	// 'disabled'     => array(),      // list of not allowed commands
 	// 'dotFiles'     => false,        // display dot files
 	// 'dirSize'      => true,         // count total directories sizes
@@ -53,13 +56,13 @@ $opts = array(
 	// 	'rm'     => true
 	// 	),
 	// 'perms'        => array(),      // individual folders/files permisions    
-	'perms' => array(
-		'/.+\.pdf$/' => array(
-			'read' => false,
-			'rm' => false
-			),
-		'/^Images\/.+/' => array('read' => false)
-		), 
+	// 'perms' => array(
+	// 	'/.+\.pdf$/' => array(
+	// 		'read' => false,
+	// 		'rm' => false
+	// 		),
+	// 	'/^Images\/.+/' => array('read' => false)
+	// 	), 
 	// 'debug'        => true,         // send debug to client
 	// 'archiveMimes' => array(),      // allowed archive's mimetypes to create. Leave empty for all available types.
 	// 'archivers'    => array()       // info about archivers to use. See example below. Leave empty for auto detect
