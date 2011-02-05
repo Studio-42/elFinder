2d9a8d814880560705aeef5b6a33ee69b59f322d
Dmitry Levashov <dio@std42.ru>
2011-02-05 19:20:19 +0300

    add unbind and one methods

--- src/js/elFinder.js
+++ src/js/elFinder.js
@@ -199,104 +199,11 @@
 		};
 		
 		
-		/**
-		 * Create/normalize event - add event.data object if not exists and
-		 * event.data.elfinder - current elfinder instance
-		 * 
-		 * @param  jQuery.Event|String  event or event name
-		 * @return jQuery.Event
-		 */
-		this.event = function(e, data) {
-			if (!e.type) {
-				e = $.Event(e.toLowerCase());
-			}
-			e.data = $.extend(e.data, data, { elfinder : this });
-			return e;
-		}
-		
-		/**
-		 * Add event listener
-		 * 
-		 * @param  jQuery.Event|String  event or event name
-		 * @param  Object  event handler
-		 * @return elFinder
-		 */
-		this.bind = function(e, c) {
-			if (typeof(c) == 'function') {
-				$.each(e.toLowerCase().split(/\s+/), function(i, e) {
-					if (self.listeners[e] === void(0)) {
-						self.listeners[e] = [];
-					}
-					self.listeners[e].push(c);
-				});
-			}
-			return this;
-		}
-		
-		/**
-		 * Send notification to all event subscribers
-		 *
-		 * @param  jQuery.Event|String  event or event type
-		 * @param  Object        extra parameters
-		 * @return elFinder
-		 */
-		this.trigger = function(e, d) {
-			var e = this.event(e, d),
-				l = this.listeners[e.type]||[], i;
 
-			this.debug('event-'+e.type, d);
-			// d && this.debug('event-data', d);
-
-			for (i = 0; i < l.length; i++) {
-				if (e.isPropagationStopped()) {
-					break;
-				}
-				try {
-					l[i](e);
-				} catch (ex) {
-					window.console && window.console.error && window.console.error(ex);
-				}
-			}
-			return this;
-		}
 		
-		/**
-		 * Bind keybord shortcut to keydown event
-		 *
-		 * @example
-		 *    elfinder.shortcut({ 
-		 *       pattern : 'ctrl+a', 
-		 *       description : 'Select all files', 
-		 *       callback : function(e) { ... }, 
-		 *       keypress : true|false (bind to keypress instead of keydown) 
-		 *    })
-		 *
-		 * @param  Object  shortcut config
-		 * @return elFinder
-		 */
-		this.shortcut = function(s) {
-			var p, c;
-
-			if (this.options.allowShortcuts && s.pattern && typeof(s.callback) == 'function') {
-				s.pattern = s.pattern.toUpperCase();
-				
-				if (!this.shortcut[s.pattern]) {
-					p = s.pattern.split('+');
-					c = p.pop();
-					
-					s.keyCode = codes[c] || c.charCodeAt(0);
-					if (s) {
-						s.altKey   = $.inArray('ALT', p) != -1;
-						s.ctrlKey  = $.inArray('CTRL', p) != -1;
-						s.shiftKey = $.inArray('SHIFT', p) != -1;
-						s.type     = s.keypress ? 'keypress' : 'keydown';
-						this.shortcuts[s.pattern] = s;
-						this.debug('shortcat-add', s)
-					}
-				}
-			}
-			return this;
-		}
+		
+		
+		
 		
 		/**
 		 * Produce ajax request
@@ -392,15 +299,7 @@
 			
 		}
 	
-		/**
-		 * Get/set last opened directory
-		 * 
-		 * @param  String  dir hash
-		 * @return String|undefined
-		 */
-		this.last = function(key) {
-			return this.options.rememberLastDir ? this.cookie('el-finder-last', key) : void(0);
-		}
+
 		
 		this.resize = function(w, h) {
 			
@@ -462,22 +361,6 @@
 			return this.cd(this.cwd.hash, true);
 		}
 		
-		/**
-		 * Open file or directory
-		 * 
-		 * @param  String  file/dir hash
-		 * @return elFinder
-		 */
-		this.open = function(hash) {
-			if (!this.locks.ui) {
-				if (this.cdc[hash] && this.cdc[hash].mime != 'directory') {
-					this.log('open file')
-				} else {
-					this.cd(hash);
-				}
-			}
-			return this;
-		}
 		
 		/**
 		 * Go to previous opened directory
@@ -655,57 +538,36 @@
 		this.cd(this.last() || '', true, true);
 		this.trigger('focus')
 		// cookie(this.cookies.view, 'list')
-		window.console.timeEnd('create')
+		this.timeEnd('create')
 	}
 	
-	elFinder.prototype.log = function(m) {
-		window.console && window.console.log && window.console.log(m);
-	}
 	
-	elFinder.prototype.debug = function(type, m) {
-		var d = this.options.debug;
-		
-		if (d == 'all' || d === true || ($.isArray(d) && $.inArray(type, d) != -1)) {
-			window.console && window.console.log && window.console.log('elfinder debug: ['+type+'] ', m);
-		} 
-		return this;
-	}
-	
-	elFinder.prototype.time = function(l) {
-		window.console && window.console.time && window.console.time(l);
-	}
-	
-	elFinder.prototype.timeEnd = function(l) {
-		window.console && window.console.timeEnd && window.console.timeEnd(l);
-	}
-	
-	elFinder.prototype.viewType = function(t) {
-		var c = 'el-finder-view';
-		
-		if (t && /^icons|list$/i.test(t)) {
-			this.cookie(c, (this._view = t));
-		} else if (!this._view) {
-			t = this.cookie(c);
-			return /^icons|list$/i.test(t) ? t : 'icons'
-		}
-		return this._view;
-	}
-	
-	elFinder.prototype.cookie = function(name, value) {
-		if (value === void(0)) {
-			if (document.cookie && document.cookie != '') {
-				var i, c = document.cookie.split(';');
-				name += '=';
-				for (i=0; i<c.length; i++) {
-					c[i] = $.trim(c[i]);
-					if (c[i].substring(0, name.length) == name) {
-						return decodeURIComponent(c[i].substring(name.length));
+	elFinder.prototype = {
+		/**
+		 * Get/set cookie
+		 *
+		 * @param  String       cookie name
+		 * @param  String|void  cookie value
+		 * @return Strng|void
+		 */
+		cookie : function(name, value) {
+			var d, o;
+
+			if (value === void(0)) {
+				if (document.cookie && document.cookie != '') {
+					var i, c = document.cookie.split(';');
+					name += '=';
+					for (i=0; i<c.length; i++) {
+						c[i] = $.trim(c[i]);
+						if (c[i].substring(0, name.length) == name) {
+							return decodeURIComponent(c[i].substring(name.length));
+						}
 					}
 				}
-			}
-			return '';
-		} else {
-			var d, o = $.extend({}, this.options.cookie);
+				return '';
+			} 
+
+			o = $.extend({}, this.options.cookie);
 			if (value===null) {
 				value = '';
 				o.expires = -1;
@@ -716,17 +578,227 @@
 				o.expires = d;
 			}
 			document.cookie = name+'='+encodeURIComponent(value)+'; expires='+o.expires.toUTCString()+(o.path ? '; path='+o.path : '')+(o.domain ? '; domain='+o.domain : '')+(o.secure ? '; secure' : '');
-		}
+		},
+		
+		/**
+		 * Get/set view type (icons | list)
+		 *
+		 * @param  String|void  type
+		 * @return Strng
+		 */
+		viewType : function(t) {
+			var c = 'el-finder-view';
+
+			if (t && /^icons|list$/i.test(t)) {
+				this.cookie(c, (this._view = t));
+			} else if (!this._view) {
+				t = this.cookie(c);
+				return /^icons|list$/i.test(t) ? t : 'icons'
+			}
+			return this._view;
+		},
+		
+		/**
+		 * Get/set last opened directory
+		 * 
+		 * @param  String  dir hash
+		 * @return String|undefined
+		 */
+		last : function(key) { return this.options.rememberLastDir ? this.cookie('el-finder-last', key) : void(0); },
+		
+		/**
+		 * Open file or directory
+		 * 
+		 * @param  String  file/dir hash
+		 * @return elFinder
+		 */
+		open : function(hash) {
+			if (!this.locks.ui) {
+				if (this.cdc[hash] && this.cdc[hash].mime != 'directory') {
+					this.log('open file')
+				} else {
+					this.cd(hash);
+				}
+			}
+			return this;
+		},
+		
+		/**
+		 * Create/normalize event - add event.data object if not exists and
+		 * event.data.elfinder - current elfinder instance
+		 * 
+		 * @param  jQuery.Event|String  event or event name
+		 * @return jQuery.Event
+		 */
+		event : function(e, data) {
+			if (!e.type) {
+				e = $.Event(e.toLowerCase());
+			}
+			e.data = $.extend(e.data, data, { elfinder : this });
+			return e;
+		},
+		
+		/**
+		 * Add event listener
+		 * 
+		 * @param  String  event(s) name(s)
+		 * @param  Object  event handler
+		 * @return elFinder
+		 */
+		bind : function(e, c) {
+			var e = e.toLowerCase().split(/\s+/), i;
+			
+			if (typeof(c) == 'function') {
+				for (i = 0; i < e.length; i++) {
+					if (this.listeners[e[i]] === void(0)) {
+						this.listeners[e[i]] = [];
+					}
+					this.listeners[e[i]].push(c);
+				}
+			}
+			return this;
+		},
+		
+		/**
+		 * Remove event listener if exists
+		 *
+		 * @param  String    event name
+		 * @param  Function  callback
+		 * @return elFinder
+		 */
+		unbind : function(e, c) {
+			var l = this.listeners[e.toLowerCase()] || [],
+				i = l.indexOf(c);
+
+			i > -1 && l.splice(i, 1);
+			return this;
+		},
+		
+		/**
+		 * Bind callback to event(s) The callback is executed at most once per event.
+		 * To bind multiply events at once, separate events names by space
+		 *
+		 * @param  String    event name
+		 * @param  Function  callback
+		 * @return elFinder
+		 */
+		one : function(e, c) {
+			var self = this,
+				h = $.proxy(c, function(e) {
+					setTimeout(function() {self.unbind(e.type, h);}, 3);
+					return c.apply(this, arguments);
+				});
+			return this.bind(e, h);
+		},
+		
+		/**
+		 * Send notification to all event subscribers
+		 *
+		 * @param  jQuery.Event|String  event or event type
+		 * @param  Object        extra parameters
+		 * @return elFinder
+		 */
+		trigger : function(e, d) {
+			var e = this.event(e, d),
+				l = this.listeners[e.type]||[], i;
+
+			this.debug('event-'+e.type, d);
+
+			for (i = 0; i < l.length; i++) {
+				if (e.isPropagationStopped()) {
+					break;
+				}
+				try {
+					l[i](e);
+				} catch (ex) {
+					window.console && window.console.error && window.console.error(ex);
+				}
+			}
+			return this;
+		},
+		
+		/**
+		 * Bind keybord shortcut to keydown event
+		 *
+		 * @example
+		 *    elfinder.shortcut({ 
+		 *       pattern : 'ctrl+a', 
+		 *       description : 'Select all files', 
+		 *       callback : function(e) { ... }, 
+		 *       keypress : true|false (bind to keypress instead of keydown) 
+		 *    })
+		 *
+		 * @param  Object  shortcut config
+		 * @return elFinder
+		 */
+		shortcut : function(s) {
+			var p, c;
+
+			if (this.options.allowShortcuts && s.pattern && typeof(s.callback) == 'function') {
+				s.pattern = s.pattern.toUpperCase();
+				
+				if (!this.shortcut[s.pattern]) {
+					p = s.pattern.split('+');
+					c = p.pop();
+					
+					s.keyCode = this.keyCodes[c] || c.charCodeAt(0);
+					if (s) {
+						s.altKey   = $.inArray('ALT', p)   != -1;
+						s.ctrlKey  = $.inArray('CTRL', p)  != -1;
+						s.shiftKey = $.inArray('SHIFT', p) != -1;
+						s.type     = s.keypress ? 'keypress' : 'keydown';
+						this.shortcuts[s.pattern] = s;
+						this.debug('shortcat-add', s)
+					}
+				}
+			}
+			return this;
+		},
+		
+		
+		
+		/**
+		 * Key codes for non alfanum keys
+		 *
+		 * @type Object
+		 **/
+		keyCodes : {
+			'ARROWLEFT'  : 37,
+			'ARROWUP'    : 38,
+			'ARROWRIGHT' : 39,
+			'ARROWDOWN'  : 40,
+			'ESC'        : 27,
+			'ENTER'      : 13,
+			'SPACE'      : 32,
+			'DELETE'     : 46,
+			'BACKSPACE'  : 8
+		},
+		
+		i18 : {
+			en : {
+				_translator  : '',
+				_translation : 'English localization',
+				dir          : 'ltr',
+				messages     : {}
+			}
+		},
+		
+		log : function(m) { window.console && window.console.log && window.console.log(m); },
+		
+		debug : function(type, m) {
+			var d = this.options.debug;
+
+			if (d == 'all' || d === true || ($.isArray(d) && $.inArray(type, d) != -1)) {
+				window.console && window.console.log && window.console.log('elfinder debug: ['+type+'] ', m);
+			} 
+			return this;
+		},
+		time : function(l) { window.console && window.console.time && window.console.time(l); },
+		timeEnd : function(l) { window.console && window.console.timeEnd && window.console.timeEnd(l); }
 	}
 	
-	elFinder.prototype.i18 = {
-		en : {
-			_translator  : '',
-			_translation : 'English localization',
-			dir          : 'ltr',
-			messages     : {}
-		}
-	}
+	
+	
 	
 	
 	$.fn.elfinder = function(o) {
