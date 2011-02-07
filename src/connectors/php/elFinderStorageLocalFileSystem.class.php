f8204e91fead67273a11f6b7fd3168d5b4105b53
Dmitry Levashov <dio@std42.ru>
2011-02-08 00:52:03 +0300

    move elFinder::normpath() from previous version

--- src/connectors/php/elFinderStorageLocalFileSystem.class.php
+++ src/connectors/php/elFinderStorageLocalFileSystem.class.php
@@ -3,11 +3,93 @@
 class elFinderStorageLocalFileSystem implements elFinderStorageDriver {
 	
 	/**
-	 * undocumented class variable
+	 * Object configuration
+	 *
+	 * @var array
+	 **/
+	protected $options = array();
+	
+	/**
+	 * Error message from last failed action
 	 *
 	 * @var string
 	 **/
-	protected $options = array();
+	protected $error = '';
+	
+	/**
+	 * extensions/mimetypes for _mimetypeDetect = 'internal' 
+	 *
+	 * @var array
+	 **/
+	protected $mimetypes = array(
+		// applications
+		'ai'    => 'application/postscript',
+		'eps'   => 'application/postscript',
+		'exe'   => 'application/octet-stream',
+		'doc'   => 'application/vnd.ms-word',
+		'xls'   => 'application/vnd.ms-excel',
+		'ppt'   => 'application/vnd.ms-powerpoint',
+		'pps'   => 'application/vnd.ms-powerpoint',
+		'pdf'   => 'application/pdf',
+		'xml'   => 'application/xml',
+		'odt'   => 'application/vnd.oasis.opendocument.text',
+		'swf'   => 'application/x-shockwave-flash',
+		// archives
+		'gz'    => 'application/x-gzip',
+		'tgz'   => 'application/x-gzip',
+		'bz'    => 'application/x-bzip2',
+		'bz2'   => 'application/x-bzip2',
+		'tbz'   => 'application/x-bzip2',
+		'zip'   => 'application/zip',
+		'rar'   => 'application/x-rar',
+		'tar'   => 'application/x-tar',
+		'7z'    => 'application/x-7z-compressed',
+		// texts
+		'txt'   => 'text/plain',
+		'php'   => 'text/x-php',
+		'html'  => 'text/html',
+		'htm'   => 'text/html',
+		'js'    => 'text/javascript',
+		'css'   => 'text/css',
+		'rtf'   => 'text/rtf',
+		'rtfd'  => 'text/rtfd',
+		'py'    => 'text/x-python',
+		'java'  => 'text/x-java-source',
+		'rb'    => 'text/x-ruby',
+		'sh'    => 'text/x-shellscript',
+		'pl'    => 'text/x-perl',
+		'sql'   => 'text/x-sql',
+		// images
+		'bmp'   => 'image/x-ms-bmp',
+		'jpg'   => 'image/jpeg',
+		'jpeg'  => 'image/jpeg',
+		'gif'   => 'image/gif',
+		'png'   => 'image/png',
+		'tif'   => 'image/tiff',
+		'tiff'  => 'image/tiff',
+		'tga'   => 'image/x-targa',
+		'psd'   => 'image/vnd.adobe.photoshop',
+		'ai'    => 'image/vnd.adobe.photoshop',
+		//audio
+
+		'mp3'   => 'audio/mpeg',
+		'mid'   => 'audio/midi',
+		'ogg'   => 'audio/ogg',
+		'mp4a'  => 'audio/mp4',
+		'wav'   => 'audio/wav',
+		'wma'   => 'audio/x-ms-wma',
+		// video
+		'avi'   => 'video/x-msvideo',
+		'dv'    => 'video/x-dv',
+		'mp4'   => 'video/mp4',
+		'mpeg'  => 'video/mpeg',
+		'mpg'   => 'video/mpeg',
+		'mov'   => 'video/quicktime',
+		'wm'    => 'video/x-ms-wmv',
+		'flv'   => 'video/x-flv',
+		'mkv'   => 'video/x-matroska'
+		);
+	
 	
 	/**
 	 * undocumented function
@@ -240,7 +322,7 @@ class elFinderStorageLocalFileSystem implements elFinderStorageDriver {
 	 * @author Dmitry (dio) Levashov
 	 **/
 	public function error() {
-		
+		return $this->$error;
 	}
 	
 	/**
@@ -253,6 +335,56 @@ class elFinderStorageLocalFileSystem implements elFinderStorageDriver {
 		
 	}
 
+	
+	/**
+	 * Return normalized path, this works the same as os.path.normpath() in Python
+	 *
+	 * @param  string  $path  path
+	 * @return string
+	 * @author Troex Nevelin
+	 **/
+	protected function normpath($path) {
+		if (empty($path)) {
+			return '.';
+		}
+
+		if (strpos($path, '/') === 0) {
+			$initial_slashes = true;
+		} else {
+			$initial_slashes = false;
+		}
+			
+		if (($initial_slashes) 
+		&& (strpos($path, '//') === 0) 
+		&& (strpos($path, '///') === false)) {
+			$initial_slashes = 2;
+		}
+			
+		$initial_slashes = (int) $initial_slashes;
+
+		$comps = explode('/', $path);
+		$new_comps = array();
+		foreach ($comps as $comp) {
+			if (in_array($comp, array('', '.'))) {
+				continue;
+			}
+				
+			if (($comp != '..') 
+			|| (!$initial_slashes && !$new_comps) 
+			|| ($new_comps && (end($new_comps) == '..'))) {
+				array_push($new_comps, $comp);
+			} elseif ($new_comps) {
+				array_pop($new_comps);
+			}
+		}
+		$comps = $new_comps;
+		$path = implode('/', $comps);
+		if ($initial_slashes) {
+			$path = str_repeat('/', $initial_slashes) . $path;
+		}
+		
+		return $path ? $path : '.';
+	}
 	
 }
 
