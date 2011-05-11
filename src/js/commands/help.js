c61d83b75489aeb79d8f079be59ba901c8598ca9
Dmitry Levashov <dio@std42.ru>
2011-05-12 01:43:01 +0400

    client: update up/home commands. add 'help' commands (placeholder)

---
+++ src/js/commands/help.js
@@ -0,0 +1,17 @@
+
+
+elFinder.prototype.commands.help = function() {
+	
+	this.alwaysEnabled = true;
+	this.state = 0;
+	
+	
+	this.getstate = function() {
+		return 0;
+	}
+	
+	this._exec = function() {
+		alert('Dont panic!');
+	}
+
+}
