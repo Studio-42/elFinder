023e6a7525a9f376a030189b2a59629e71a267e6
Dmitry (dio) Levashov <dio@std42.ru>
2011-07-03 02:20:19 +0400

    clean elFinder.options

--- src/js/elFinder.options.js
+++ src/js/elFinder.options.js
@@ -38,6 +38,22 @@ elFinder.prototype.options = {
 	transport : null,
 
 	/**
+	 * Data to append to all requests and to upload files
+	 *
+	 * @type Object
+	 * @default  {}
+	 */
+	customData : {token : '42', test : 'test'},
+	
+	/**
+	 * Event listeners to bind on elFinder init
+	 *
+	 * @type Object
+	 * @default  {}
+	 */
+	handlers : {},
+
+	/**
 	 * Interface language
 	 *
 	 * @type String
@@ -52,195 +68,18 @@ elFinder.prototype.options = {
 	 */
 	cssClass : '',
 
-	commands : ['open', 'reload', 'home', 'up', 'back', 'forward', 'getfile', 'quicklook', 'download', 'rm', 'duplicate', 'rename', 'mkdir', 'mkfile', 'upload', 'copy', 'cut', 'paste', 'edit', 'extract', 'archive', 'search', 'info', 'view', 'help'],
-	
-	getFileCallback : null,
 	/**
-	 * UI plugins to load.
-	 * Current dir ui and dialogs loads always.
-	 * Here set not required plugins as folders tree/toolbar/statusbar etc.
+	 * Active commands list
+	 * If some required commands will be missed here, elFinder will add its
 	 *
 	 * @type Array
-	 * @default ['toolbar', 'tree']
 	 */
-	ui : ['toolbar', 'places', 'tree', 'path', 'stat'],
-
-	/**
-	 * Some UI plugins options.
-	 *
-	 * @type Object
-	 */
-	uiOptions : {
-		// toolbar content
-		toolbar : [
-			['back', 'forward'],
-			['reload'],
-			['home', 'up'],
-			// ['reload', 'up', 'home'],
-			['mkdir', 'mkfile', 'upload'],
-			['open', 'download', 'getfile'],
-			['info'],
-			['quicklook'],
-			['copy', 'cut', 'paste'],
-			['rm'],
-			['duplicate', 'rename', 'edit'],
-			['extract', 'archive'],
-			['search'],
-			['view'],
-			['help']
-		]
-		// folders tree options
-		// tree : {tag : 'ul'}
-	},
-
-	/**
-	 * Display only required file by types
-	 *
-	 * @type Array
-	 * @default []
-	 * @example
-	 *  onlyMimes : ["image"] - display all images
-	 *  onlyMimes : ["image/png", "application/x-shockwave-flash"] - display png and flash
-	 */
-	onlyMimes : [],
-
-	/**
-	 * How to sort files in current directory
-	 *
-	 * @type String
-	 * @default "nameDirsFirst"
-	 * @example
-	 *  - sort : 'nameDirsFirst' - sort by name, directory first
-	 *  - sort : 'kindDirsFirst' - sort by kind, name, directory first
-	 *  - sort : 'sizeDirsFirst' - sort by size, name, directory first
-	 *  - sort : 'name' - sort by name
-	 *  - sort : 'kind' - sort by kind, name
-	 *  - sort : 'size' - sort by size, name
-	 */
-	sort : 'kindDirsFirst',
+	commands : [
+		'open', 'reload', 'home', 'up', 'back', 'forward', 'getfile', 'quicklook', 
+		'download', 'rm', 'duplicate', 'rename', 'mkdir', 'mkfile', 'upload', 'copy', 
+		'cut', 'paste', 'edit', 'extract', 'archive', 'search', 'info', 'view', 'help'
+	],
 	
-	/**
-	 * Show nav panel (not implemented yet)
-	 *
-	 * @type Boolean
-	 * @default true
-	 */
-	allowNav : true,
-	/**
-	 * On init expand current root directory in nav
-	 *
-	 * @type Boolean
-	 * @default true
-	 */
-	openRootOnLoad : true,
-	
-	resizable : true,
-	
-	notifyDelay : 500,
-	
-	syncTree : true,
-
-	/* width */
-	width          : 'auto',
-	/* height */
-	height         : 415,
-	/* disable shortcuts exclude arrows/space */
-	allowShortcuts : true,
-	/* open last visited dir after reload page or close and open browser */
-	rememberLastDir : true,
-	/**
-	 * Data to append to all ajax requests and to upload form
-	 *
-	 * @type Object
-	 * @default  {}
-	 */
-	customData : {token : '42', test : 'test'},
-	
-	handlers : {},
-	
-	// getFileCallback : function(file) {
-	// 	console.log('got file')
-	// 	console.log(file)
-	// },
-	
-	/**
-	 * Show toolbar?
-	 *
-	 * @type Boolean
-	 * @default  true
-	 */
-	allowToolbar : true,
-	/**
-	 * Show navbar?
-	 * If set to false - navbar does not shown and dir tree doesnot created
-	 *
-	 * @type Boolean
-	 * @default  true
-	 */
-	allowNavbar : true,
-	/**
-	 * Show statusbar?
-	 *
-	 * @type Boolean
-	 * @default  true
-	 */
-	// allowStatusbar : true,
-	/**
-	 * Lazy load config.
-	 * How many files show at once?
-	 *
-	 * @type Number
-	 * @default  50
-	 */
-	showFiles : 30,
-	/**
-	 * Lazy load config.
-	 * Distance in px to cwd bottom edge to start display files
-	 *
-	 * @type Number
-	 * @default  50
-	 */
-	showThreshold : 50,
-	
-	/**
-	 * Additional rule to valid new file name.
-	 * By default not allowed empty names or '..'
-	 *
-	 * @type false|RegExp|function
-	 * @default  false
-	 * @example
-	 *  disable names with spaces:
-	 *  validName : /^[^\s]$/
-	 */
-	validName : false,
-	
-	sync : 0,
-	
-	// commands : ['back', 'forward', 'reload', 'up', 'home', 'open', 'copy', 'cut', 'paste', 'rm', 'info', 'duplicate', 'rename'],
-	
-	/**
-	 * How many thumbnails create in one request
-	 *
-	 * @type Number
-	 * @default  5
-	 */
-	loadTmbs : 5,
-	
-	/* cookie options */
-	cookie         : {
-		expires : 30,
-		domain  : '',
-		path    : '/',
-		secure  : false
-	},
-	
-	/* contextmenu commands */
-	contextmenu : {
-		navbar : ['open', '|', 'copy', 'cut', 'paste', 'duplicate', '|', 'rm', '|', 'info'],
-		cwd    : ['reload', 'back', '|', 'upload', 'mkdir', 'mkfile', 'paste', '|', 'search', '|', 'info'],
-		files  : ['getfile', '|','open', 'quicklook', '|', 'download', '|', 'edit', '|', 'copy', 'cut', 'paste', 'duplicate', '|', 'rm', '|', 'archive', 'extract', '|', 'info']
-	},
-
 	/**
 	 * Commands options.
 	 *
@@ -248,6 +87,7 @@ elFinder.prototype.options = {
 	 **/
 	commandsOptions : {
 		// "getfile" command options.
+		// @todo  option to close after callback
 		getfile : {
 			// allow to return multiple files info
 			multiple : false,
@@ -255,6 +95,7 @@ elFinder.prototype.options = {
 			folders  : false
 		},
 		// "upload" command options.
+		// @todo split forceIframe & transport
 		upload : {
 			// open dialog on click toolbar button instead of open browser select files dialog
 			forceDialog   : false,
@@ -280,6 +121,210 @@ elFinder.prototype.options = {
 		}
 	},
 	
+	/**
+	 * Callback for "getfile" commands.
+	 * Required to use elFinder with WYSIWYG editors etc..
+	 *
+	 * @type Function
+	 * @default null (command not active)
+	 */
+	getFileCallback : null,
+	
+	/**
+	 * UI plugins to load.
+	 * Current dir ui and dialogs loads always.
+	 * Here set not required plugins as folders tree/toolbar/statusbar etc.
+	 *
+	 * @type Array
+	 * @default ['toolbar', 'places', 'tree', 'path', 'stat']
+	 */
+	ui : ['toolbar', 'places', 'tree', 'path', 'stat'],
+	
+	/**
+	 * Some UI plugins options.
+	 * @todo openRootOnLoad/syncTree -> tree option
+	 * @type Object
+	 */
+	uiOptions : {
+		// toolbar configuration
+		toolbar : [
+			['back', 'forward'],
+			['reload'],
+			['home', 'up'],
+			['mkdir', 'mkfile', 'upload'],
+			['open', 'download', 'getfile'],
+			['info'],
+			['quicklook'],
+			['copy', 'cut', 'paste'],
+			['rm'],
+			['duplicate', 'rename', 'edit'],
+			['extract', 'archive'],
+			['search'],
+			['view'],
+			['help']
+		]
+	},
+
+	/**
+	 * On init expand current root directory in nav
+	 *
+	 * @type Boolean
+	 * @default true
+	 */
+	openRootOnLoad : true,
+	syncTree : true,
+	
+	/**
+	 * Display only required files by types
+	 *
+	 * @type Array
+	 * @default []
+	 * @example
+	 *  onlyMimes : ["image"] - display all images
+	 *  onlyMimes : ["image/png", "application/x-shockwave-flash"] - display png and flash
+	 */
+	onlyMimes : [],
+
+	/**
+	 * How to sort files in current directory
+	 *
+	 * @type String
+	 * @default "nameDirsFirst"
+	 * @example
+	 *  - sort : 'nameDirsFirst' - sort by name, directory first
+	 *  - sort : 'kindDirsFirst' - sort by kind, name, directory first
+	 *  - sort : 'sizeDirsFirst' - sort by size, name, directory first
+	 *  - sort : 'name' - sort by name
+	 *  - sort : 'kind' - sort by kind, name
+	 *  - sort : 'size' - sort by size, name
+	 */
+	sort : 'kindDirsFirst',
+	
+	/**
+	 * elFinder width
+	 *
+	 * @type String|Number
+	 * @default  "auto"
+	 */
+	width : 'auto',
+	
+	/**
+	 * elFinder height
+	 *
+	 * @type Number
+	 * @default  "auto"
+	 */
+	height : 415,
+	
+	/**
+	 * Make elFinder resizable if jquery ui resizable available
+	 *
+	 * @type Boolean
+	 * @default  true
+	 */
+	resizable : true,
+	
+	/**
+	 * Timeout before open notifications dialogs
+	 *
+	 * @type Number
+	 * @default  500 (.5 sec)
+	 */
+	notifyDelay : 500,
+	
+	/**
+	 * Allow shortcuts
+	 *
+	 * @type Boolean
+	 * @default  true
+	 */
+	allowShortcuts : true,
+	
+	/**
+	 * Remeber last opened dir to open it after reload or in next session
+	 *
+	 * @type Boolean
+	 * @default  true
+	 */
+	rememberLastDir : true,
+	
+	/**
+	 * Lazy load config.
+	 * How many files display at once?
+	 *
+	 * @type Number
+	 * @default  50
+	 */
+	showFiles : 30,
+	
+	/**
+	 * Lazy load config.
+	 * Distance in px to cwd bottom edge to start display files
+	 *
+	 * @type Number
+	 * @default  50
+	 */
+	showThreshold : 50,
+	
+	/**
+	 * Additional rule to valid new file name.
+	 * By default not allowed empty names or '..'
+	 *
+	 * @type false|RegExp|function
+	 * @default  false
+	 * @example
+	 *  disable names with spaces:
+	 *  validName : /^[^\s]$/
+	 */
+	validName : false,
+	
+	/**
+	 * Sync content interval
+	 * @todo - fix in elFinder
+	 * @type Number
+	 * @default  0 (do not sync)
+	 */
+	sync : 1000,
+	
+	/**
+	 * How many thumbnails create in one request
+	 *
+	 * @type Number
+	 * @default  5
+	 */
+	loadTmbs : 5,
+	
+	/**
+	 * Cookie option for browsersdoes not suppot localStorage
+	 *
+	 * @type Object
+	 */
+	cookie         : {
+		expires : 30,
+		domain  : '',
+		path    : '/',
+		secure  : false
+	},
+	
+	/**
+	 * Contextmenu config
+	 *
+	 * @type Object
+	 */
+	contextmenu : {
+		// navbarfolder menu
+		navbar : ['open', '|', 'copy', 'cut', 'paste', 'duplicate', '|', 'rm', '|', 'info'],
+		// current directory menu
+		cwd    : ['reload', 'back', '|', 'upload', 'mkdir', 'mkfile', 'paste', '|', 'search', '|', 'info'],
+		// current directory file menu
+		files  : ['getfile', '|','open', 'quicklook', '|', 'download', '|', 'edit', '|', 'copy', 'cut', 'paste', 'duplicate', '|', 'rm', '|', 'archive', 'extract', '|', 'info']
+	},
+
+	/**
+	 * Debug config
+	 *
+	 * @type Array|Boolen
+	 */
 	// debug : true
 	debug : ['error', 'warning', 'event-searchend', 'event-viewchange']
 }
