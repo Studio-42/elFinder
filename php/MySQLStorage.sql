77806adbf1f50f20b3ecb46bc0a124e38274c74f
Troex Nevelin <troex@fury.scancode.ru>
2012-03-03 19:29:38 +0200

    use general collcation instead unicode, improve mysql search speed

--- php/MySQLStorage.sql
+++ php/MySQLStorage.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS `elfinder_file`;
 CREATE TABLE IF NOT EXISTS `elfinder_file` (
   `id`        int(7) unsigned NOT NULL auto_increment,
   `parent_id` int(7) unsigned NOT NULL,
-  `name`      varchar(256) collate utf8_unicode_ci NOT NULL,
+  `name`      varchar(256) NOT NULL,
   `content`   longblob NOT NULL,
   `size`      int(10) unsigned NOT NULL default '0',
   `mtime`     int(10) unsigned NOT NULL,
-  `mime`      varchar(256) collate utf8_unicode_ci NOT NULL default 'unknown',
+  `mime`      varchar(256) NOT NULL default 'unknown',
   `read`      enum('1', '0') NOT NULL default '1',
   `write`     enum('1', '0') NOT NULL default '1',
   `locked`    enum('1', '0') NOT NULL default '0',
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `elfinder_file` (
   PRIMARY KEY (`id`),
   UNIQUE KEY  `parent_name` (`parent_id`, `name`),
   KEY         `parent_id`   (`parent_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 INSERT INTO `elfinder_file`
 (`id`, `parent_id`, `name`,     `content`, `size`, `mtime`, `mime`,      `read`, `write`, `locked`, `hidden`, `width`, `height`) VALUES 
