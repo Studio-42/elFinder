63e84c5e36503c9b3404b29a11842776db5af953
Dmitry (dio) Levashov <dio@std42.ru>
2012-03-11 22:42:25 +0400

    dummy commit

--- php/connector.minimal.php
+++ php/connector.minimal.php
@@ -18,12 +18,12 @@ include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeLocalFileSyste
  *
  * @param  string  $attr  attribute name (read|write|locked|hidden)
  * @param  string  $path  file path relative to volume root directory started with directory separator
- * @return bool
+ * @return bool|null
  **/
 function access($attr, $path, $data, $volume) {
 	return strpos(basename($path), '.') === 0       // if file/folder begins with '.' (dot)
 		? !($attr == 'read' || $attr == 'write')    // set read+write to false, other (locked+hidden) set to true
-		:  ($attr == 'read' || $attr == 'write');   // else set read+write to true, locked+hidden to false
+		:  null;                                    // else elFinder decide it itself
 }
 
 $opts = array(
