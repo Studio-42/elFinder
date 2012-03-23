1050ed86791d11511c7dbdca642b458b5d952c86
nao-pon <hypweb@gmail.com>
2012-03-23 10:34:07 +0900

    Enable when clipboard is empty for switch to upload.

--- js/commands/paste.js
+++ js/commands/paste.js
@@ -34,7 +34,8 @@ elFinder.prototype.commands.paste = function() {
 			dst = this.fm.cwd();
 		}
 
-		return this.fm.clipboard().length && dst.mime == 'directory' && dst.write ? 0 : -1;
+		//return this.fm.clipboard().length && dst.mime == 'directory' && dst.write ? 0 : -1;
+		return dst.mime == 'directory' && dst.write ? 0 : -1;
 	}
 	
 	this.exec = function(dst) {
@@ -175,7 +176,10 @@ elFinder.prototype.commands.paste = function() {
 			parents, fparents;
 
 
-		if (!cnt || !dst || dst.mime != 'directory') {
+		if (!cnt) {
+			if (dst && dst.mime == 'directory') {
+				fm.exec('upload');
+			}
 			return dfrd.reject();
 		}
 			
