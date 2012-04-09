425e3122dd56cc60d2c574af6a702a98fe8712c4
Dmitry (dio) Levashov <dio@std42.ru>
2012-04-09 20:54:51 +0400

    do nothing

--- php/connector.php
+++ php/connector.php
@@ -225,7 +225,7 @@ $opts = array(
 		array(
 			'driver'     => 'LocalFileSystem',
 			'path'       => '../files/',
-			'startPath'  => '../files/icons/',
+			'startPath'  => '../files/test/',
 			'URL'        => dirname($_SERVER['PHP_SELF']) . '/../files/',
 			// 'alias'      => 'File system',
 			'mimeDetect' => 'internal',
