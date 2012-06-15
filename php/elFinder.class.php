332cb349ecc0d1a66a020328fa02385c5107c81e
nao-pon <hypweb@gmail.com>
2012-06-15 22:17:19 +0900

    correct code formatting

--- php/elFinder.class.php
+++ php/elFinder.class.php
@@ -875,7 +875,9 @@ class elFinder {
 			$errno,$errstr,$connect_timeout);
 			if ($fp) break;
 			$connect_try_count++;
-			if (connection_aborted()) exit();
+			if (connection_aborted()) {
+				exit();
+			}
 			sleep(1); // wait 1sec
 		}
 		
@@ -895,7 +897,9 @@ class elFinder {
 		
 		$_response = true;
 		while ($_response && (is_null($getSize) || strlen($response) < $getSize)) {
-			if (connection_aborted()) exit();
+			if (connection_aborted()) {
+				exit();
+			}
 			if ($_response = fread($fp, $readsize)) {
 				$response .= $_response;
 			}
