1644251eacb1c859f0938f729ab1fb3bf39de76c
Yuri Khrustalev <ykhrustalev@ykhrustalev.parallels.com>
2013-03-19 12:30:24 +0400

    Adding archive/extract functionality to FTP driver on GNU/Linux Adding localization for new error messages for archive/extract commands Fixing sort localization Fixing defect with temporary path directory permissions Fixing defect with cacheDir function hiding files in folders with space in name Fixing defect with mount function Fixed overwrite dialog for extract command

--- js/elFinder.js
+++ js/elFinder.js
@@ -1116,7 +1116,8 @@ window.elFinder = function(node, opts) {
 			dfrd.reject(error);
 			error && self.request({
 				data   : {cmd : 'open', target : self.lastDir(''), tree : 1, init : 1},
-				notify : {type : 'open', cnt : 1, hideCnt : true}
+				notify : {type : 'open', cnt : 1, hideCnt : true},
+				preventDefault : true
 			});
 		})
 		.done(function(odata, pdata) {
--- js/i18n/elfinder.en.js
+++ js/i18n/elfinder.en.js
@@ -80,6 +80,14 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'errNetMountNoDriver'  : 'Unsupported protocol.',     // added 17.04.2012
 			'errNetMountFailed'    : 'Mount failed.',         // added 17.04.2012
 			'errNetMountHostReq'   : 'Host required.', // added 18.04.2012
+			'errSessionExpires'    : 'Your session has expired due to inactivity.',
+			'errCreatingTempDir'   : 'Unable to create temporary directory: "$1"',
+			'errFtpDownloadFile'   : 'Unable to download file from FTP: "$1"',
+			'errFtpUploadFile'     : 'Unable to upload file to FTP: "$1"',
+			'errFtpMkdir'          : 'Unable to create remote directory on FTP: "$1"',
+			'errArchiveExec'       : 'Error while archiving files: "$1"',
+			'errExtractExec'       : 'Error while extracting files: "$1"',
+
 			/******************************* commands names ********************************/
 			'cmdarchive'   : 'Create archive',
 			'cmdback'      : 'Back',
@@ -137,9 +145,9 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'ntfextract'  : 'Extracting files from archive',
 			'ntfsearch'   : 'Searching files',
 			'ntfresize'   : 'Resizing images',
-			'ntfsmth'     : 'Doing something >_<',
-      		'ntfloadimg'  : 'Loading image',
-      		'ntfnetmount' : 'Mounting network volume', // added 18.04.2012
+			'ntfsmth'     : 'Doing something',
+			'ntfloadimg'  : 'Loading image',
+			'ntfnetmount' : 'Mounting network volume', // added 18.04.2012
 			
 			/************************************ dates **********************************/
 			'dateUnknown' : 'unknown',
