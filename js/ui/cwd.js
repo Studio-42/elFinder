40d30e226da3dc27355921cb07abe8dee003211f
nao-pon <hypweb@gmail.com>
2013-12-13 20:30:39 +0900

    [ui,cwd] fix hover of cwd

--- js/ui/cwd.js
+++ js/ui/cwd.js
@@ -791,8 +791,9 @@ $.fn.elfindercwd = function(fm, options) {
 				.delegate(fileSelector, 'scrolltoview', function() {
 					scrollToView($(this));
 				})
-				.delegate(fileSelector, 'hover', function(e) {
+				.delegate(fileSelector, 'mouseenter.'+fm.namespace+' mouseleave.'+fm.namespace, function(e) {
 					fm.trigger('hover', {hash : $(this).attr('id'), type : e.type});
+					$(this).toggleClass('ui-state-hover');
 				})
 				.bind('contextmenu.'+fm.namespace, function(e) {
 					var file = $(e.target).closest('.'+clFile);
