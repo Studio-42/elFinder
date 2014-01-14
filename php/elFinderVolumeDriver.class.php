9a1bd26690de835e5d3cd69344f94022a0b81811
nao-pon <hypweb@gmail.com>
2014-01-14 14:29:38 +0900

    [php:driver] add optional parameter `$lineal` into parents()
    `@param  bool|null $lineal only lineal parents`

--- php/elFinderVolumeDriver.class.php
+++ php/elFinderVolumeDriver.class.php
@@ -1105,11 +1105,12 @@ abstract class elFinderVolumeDriver {
 	/**
 	 * Return part of dirs tree from required dir up to root dir
 	 *
-	 * @param  string  $hash  directory hash
+	 * @param  string    $hash   directory hash
+	 * @param  bool|null $lineal only lineal parents
 	 * @return array
 	 * @author Dmitry (dio) Levashov
 	 **/
-	public function parents($hash) {
+	public function parents($hash, $lineal = false) {
 		if (($current = $this->dir($hash)) == false) {
 			return false;
 		}
@@ -1125,7 +1126,7 @@ abstract class elFinderVolumeDriver {
 			}
 			
 			array_unshift($tree, $stat);
-			if ($path != $this->root) {
+			if (!$lineal && ($path != $this->root)) {
 				foreach ($this->gettree($path, 0) as $dir) {
 					if (!in_array($dir, $tree)) {
 						$tree[] = $dir;
