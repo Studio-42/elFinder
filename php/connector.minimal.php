e17368cc0e3827e7af64293c93f18110670a1b26
faraz <faraz@adeogroup.co.uk>
2014-04-15 13:15:55 +0100

    updated to connector minimal

--- php/connector.minimal.php
+++ php/connector.minimal.php
@@ -48,11 +48,24 @@ $opts = array(
 	'roots' => array(
 		array(
 			'driver'        => 'LocalFileSystem',   // driver for accessing file system (REQUIRED)
-			'path'          => '../files/',         // path to files (REQUIRED)
-			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
+			'path'          => $_SERVER['DOCUMENT_ROOT'] . '/img/',         // path to files (REQUIRED)
+			'URL'           => 'img', // URL to files (REQUIRED)
 			'accessControl' => 'access'             // disable and hide dot starting files (OPTIONAL)
 		)
-	)
+	),
+    'bind' => array(
+        'upload.presave' => array(
+            'Plugin.AutoResize.onUpLoadPreSave'
+        )
+    ),
+    'plugin' => array(
+        'AutoResize' => array(
+            'enable' => true,
+            'maxWidth'  => 1200,
+            'maxHeight'  => 1200,
+            'quality' => 95
+        )
+    )
 );
 
 // run elFinder
