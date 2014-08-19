Studio-42/elFinder

commit f38a37870d2c25381f48e34b2de0081507ea91b8

Change http request in 'open' command from GET to POST to hide url custom parameters (like passwords)

js/commands/open.js
@@ -79,9 +79,30 @@ elFinder.prototype.commands.open = function() {
 				w = 'width='+parseInt(2*$(window).width()/3)+',height='+parseInt(2*$(window).height()/3);
 			}
 
-			if (!window.open(url, '_blank', w + ',top=50,left=50,scrollbars=yes,resizable=yes')) {
+			var wnd = window.open('', 'new_window', w + ',top=50,left=50,scrollbars=yes,resizable=yes');
+			if (!wnd) {
 				return dfrd.reject('errPopup');
 			}
+			
+			var form = document.createElement("form");
+			form.action = fm.options.url;
+			form.method = 'POST';
+			form.target = 'new_window';
+			form.style.display = 'none';
+			var params = $.extend({}, fm.options.customData, {
+				cmd: 'file',
+				target: file.hash
+			});
+			$.each(params, function(key, val)
+			{
+				var input = document.createElement("input");
+				input.name = key;
+				input.value = val;
+				form.appendChild(input);
+			});
+			
+			document.body.appendChild(form);
+			form.submit();
 		}
 		return dfrd.resolve(hashes);
 	}

commit 60513fd1feab4e3e76cb8c2266e2112dddab3c8c

regression from f38a3787 : we should use variable url which was set a few lines upper. Otherwise elfinder always opens files through connector 'file' command, even if we want to open files directly from remote URL.

js/commands/open.js
@@ -85,7 +85,7 @@ elFinder.prototype.commands.open = function() {
 			}
 			
 			var form = document.createElement("form");
-			form.action = fm.options.url;
+			form.action = url;
 			form.method = 'POST';
 			form.target = 'new_window';
 			form.style.display = 'none';