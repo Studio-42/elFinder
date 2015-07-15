74c85ab7aabc3b59ff752b3a5bda375c39884526
nao-pon <hypweb@gmail.com>
2015-07-15 10:47:25 +0900

    [VD:abstract] fix `.js` textfile detection with `finfo`

--- php/elFinderVolumeDriver.class.php
+++ php/elFinderVolumeDriver.class.php
@@ -2855,6 +2855,8 @@ abstract class elFinderVolumeDriver {
 				$ext = (false === $pos = strrpos($name, '.')) ? '' : substr($name, $pos + 1);
 				if ($ext && preg_match('~^application/(?:octet-stream|(?:x-)?zip)~', $type)) {
 					if (isset(elFinderVolumeDriver::$mimetypes[$ext])) $type = elFinderVolumeDriver::$mimetypes[$ext];
+				} else if ($ext === 'js' && preg_match('~^text/~', $type)) {
+					$type = 'text/javascript';
 				}
 			} else {
 				$type = 'unknown';
