65e22ac565491b4a43af785317be97ddf7e34b3d
nao-pon <hypweb@gmail.com>
2015-08-04 01:16:31 +0900

    fix #1066 problem with jQuery UI < 1.11.0 in ed8b88489ce4f31b894acf7d3dd44a68f3c47f0d

--- js/jquery.elfinder.js
+++ js/jquery.elfinder.js
@@ -1,47 +1,21 @@
 /*** jQuery UI droppable performance tune for elFinder ***/
+(function(){
+var origin = $.ui.ddmanager.prepareOffsets;
 $.ui.ddmanager.prepareOffsets = function( t, event ) {
-	
-	/*** elFinder original ***/
-	var isInView = function(elem) {
+	var isOutView = function(elem) {
 		var rect = elem.getBoundingClientRect();
-		return document.elementFromPoint(rect.left, rect.top)? true : false;
+		return document.elementFromPoint(rect.left, rect.top)? false : true;
 	}
 	
-	var i, j,
-		m = $.ui.ddmanager.droppables[ t.options.scope ] || [],
-		type = event ? event.type : null, // workaround for #2317
-		list = ( t.currentItem || t.element ).find( ":data(ui-droppable)" ).addBack();
-
-	droppablesLoop: for ( i = 0; i < m.length; i++ ) {
-
-		// No disabled and non-accepted
-		if ( m[ i ].options.disabled || ( t && !m[ i ].accept.call( m[ i ].element[ 0 ], ( t.currentItem || t.element ) ) ) ) {
-			continue;
-		}
-
-		// Filter out elements in the current dragged item
-		for ( j = 0; j < list.length; j++ ) {
-			if ( list[ j ] === m[ i ].element[ 0 ] ) {
-				m[ i ].proportions().height = 0;
-				continue droppablesLoop;
-			}
-		}
-
-		m[ i ].visible = m[ i ].element.css( "display" ) !== "none" /** elFinder **/ && isInView(m[ i ].element[ 0 ]);
-		if ( !m[ i ].visible ) {
-			continue;
-		}
-
-		// Activate the droppable if used directly from draggables
-		if ( type === "mousedown" ) {
-			m[ i ]._activate.call( m[ i ], event );
-		}
-
-		m[ i ].offset = m[ i ].element.offset();
-		m[ i ].proportions({ width: m[ i ].element[ 0 ].offsetWidth, height: m[ i ].element[ 0 ].offsetHeight });
-
+	var i, m = $.ui.ddmanager.droppables[ t.options.scope ] || [];
+	for ( i = 0; i < m.length; i++ ) {
+		m[ i ].options.disabled = isOutView(m[ i ].element[ 0 ]);
 	}
+	
+	// call origin function
+	return origin( t, event );
 };
+})();
 
 $.fn.elfinder = function(o) {
 	
