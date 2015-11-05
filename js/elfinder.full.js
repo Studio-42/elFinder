742f57732de250328aa0ef516c93b166d800bf76
nao-pon <hypweb@gmail.com>
2015-11-05 22:36:41 +0900

    release elFinder version 2.0.3

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.0.3 (2.0 Nightly: 456973c) (2015-11-05)
+ * Version 2.0.3 (2015-11-05)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
@@ -3023,7 +3023,7 @@ elFinder.prototype = {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.0.3 (2.0 Nightly: 456973c)';
+elFinder.prototype.version = '2.0.3';
 
 
 
