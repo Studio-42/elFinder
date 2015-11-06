06d30c8e6d02b96f9f8564d5628b301c5c809870
nao-pon <hypweb@gmail.com>
2015-11-06 09:32:07 +0900

    src build elFinder-2.1-7da0889

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1.0 (2.1 Nightly: 8d807ff) (2015-11-06)
+ * Version 2.1.0 (2.1 Nightly: 7da0889) (2015-11-06)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
@@ -211,10 +211,9 @@ window.elFinder = function(node, opts) {
 		/**
 		 * elfinder path for sound played on remove
 		 * @type String
-		 * @default ./
+		 * @default ./sounds/
 		 **/
-		 
-		 soundPath = './',
+		soundPath = './sounds/',
 				
 		beeper = $(document.createElement('audio')).hide().appendTo('body')[0],
 			
@@ -1820,7 +1819,7 @@ window.elFinder = function(node, opts) {
 		.bind('rm', function(e) {
 			var play  = beeper.canPlayType && beeper.canPlayType('audio/wav; codecs="1"');
 		
-			play && play != '' && play != 'no' && $(beeper).html('<source src="' + soundPath + '/sounds/rm.wav" type="audio/wav">')[0].play()
+			play && play != '' && play != 'no' && $(beeper).html('<source src="' + soundPath + 'rm.wav" type="audio/wav">')[0].play()
 		})
 		
 		;
@@ -1968,7 +1967,7 @@ window.elFinder = function(node, opts) {
 	}
 	
 	if (this.options.soundPath) {
-		soundPath = this.options.soundPath;
+		soundPath = this.options.soundPath.replace(/\/+$/, '') + '/';
 	}
 	
 	// update size	
@@ -4108,7 +4107,7 @@ if (!Object.keys) {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1.0 (2.1 Nightly: 8d807ff)';
+elFinder.prototype.version = '2.1.0 (2.1 Nightly: 7da0889)';
 
 
 
