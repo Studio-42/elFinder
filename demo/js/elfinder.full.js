bef373d38043133b9ca7815f0491e66a5426a2d4
nao-pon <hypweb@gmail.com>
2015-12-01 22:03:54 +0900

    update demo elFinder-2.1-b6646aa

--- demo/js/elfinder.full.js
+++ demo/js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1.2 (2.1 Nightly: d2191a2) (2015-12-01)
+ * Version 2.1.2 (2.1 Nightly: b6646aa) (2015-12-01)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
@@ -4420,7 +4420,7 @@ if (!Object.keys) {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1.2 (2.1 Nightly: d2191a2)';
+elFinder.prototype.version = '2.1.2 (2.1 Nightly: b6646aa)';
 
 
 
@@ -6447,9 +6447,10 @@ $.fn.elfindercontextmenu = function(fm) {
 					scrolltop  = win.scrollTop(),
 					scrollleft = win.scrollLeft(),
 					m          = fm.UA.Touch? 10 : 0,
+					zoom       = document.body.clientWidth / window.innerWidth,
 					css        = {
-						top  : (y + m + height < wheight ? y + m : y - m - height > 0 ? y - m - height : y + m) + scrolltop,
-						left : (x + m + width  < wwidth  ? x + m : x - m - width) + scrollleft,
+						top  : ((y + m + height < wheight ? y + m : y - m - height > 0 ? y - m - height : y + m) + scrolltop) / zoom,
+						left : ((x + m + width  < wwidth  ? x + m : x - m - width) / zoom + scrollleft) / zoom,
 						'z-index' : 100 + fm.getUI('workzone').zIndex()
 					};
 
