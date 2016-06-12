1dbaf8bff4ad0726c07620a26c143bec6c245133
nao-pon <hypweb@gmail.com>
2016-06-12 14:16:18 +0900

    update demo elFinder-2.1-a5904b5

--- demo/js/i18n/elfinder.LANG.js
+++ demo/js/i18n/elfinder.LANG.js
@@ -140,6 +140,7 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'cmdplaces'    : 'To Places', // added 28.12.2014
 			'cmdchmod'     : 'Change mode', // from v2.1 added 20.6.2015
 			'cmdopendir'   : 'Open a folder', // from v2.1 added 13.1.2016
+			'cmdcolwidth'  : 'Reset column width', // from v2.1.13 added 12.06.2016
 			
 			/*********************************** buttons ***********************************/
 			'btnClose'  : 'Close',
