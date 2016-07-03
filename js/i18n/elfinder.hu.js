5b36aa4d0a39095e1fb0612c5ae57a78bb3fc0be
nao-pon <hypweb@gmail.com>
2016-07-03 20:38:59 +0900

    src build elFinder-2.1-2191bcc

--- js/i18n/elfinder.hu.js
+++ js/i18n/elfinder.hu.js
@@ -1,23 +1,23 @@
 /**
- * Hungarian translation
+ * magyar translation
  * @author Gáspár Lajos <info@glsys.eu>
- * @version 2014-12-19
+ * @version 2016-06-29
  */
 if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object') {
 	elFinder.prototype.i18.hu = {
 		translator : 'Gáspár Lajos &lt;info@glsys.eu&gt;',
 		language   : 'magyar',
 		direction  : 'ltr',
-		dateFormat : 'M d, Y h:i A', // Mar 13, 2012 05:27 PM
-		fancyDateFormat : '$1 h:i A', // will produce smth like: Today 12:25 PM
+		dateFormat : 'Y.F.d H:i:s', // Mar 13, 2012 05:27 PM
+		fancyDateFormat : '$1 H:i', // will produce smth like: Today 12:25 PM
 		messages   : {
-			
+
 			/********************************** errors **********************************/
 			'error'                : 'Hiba',
 			'errUnknown'           : 'Ismeretlen hiba.',
 			'errUnknownCmd'        : 'Ismeretlen parancs.',
 			'errJqui'              : 'Hibás jQuery UI konfiguráció. A "selectable", "draggable" és a "droppable" komponensek szükségesek.',
-			'errNode'              : 'elFinder requires DOM Element to be created.',
+			'errNode'              : 'Az elFinder "DOM" elem létrehozását igényli.',
 			'errURL'               : 'Hibás elFinder konfiguráció! "URL" paraméter nincs megadva.',
 			'errAccess'            : 'Hozzáférés megtagadva.',
 			'errConnect'           : 'Nem sikerült csatlakozni a kiszolgálóhoz.',
@@ -25,46 +25,48 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'errTimeout'           : 'Kapcsolat időtúllépés.',
 			'errNotFound'          : 'A backend nem elérhető.',
 			'errResponse'          : 'Hibás backend válasz.',
-			'errConf'              : 'Invalid backend configuration.',
-			'errJSON'              : 'PHP JSON module not installed.',
-			'errNoVolumes'         : 'Readable volumes not available.',
-			'errCmdParams'         : 'Invalid parameters for command "$1".',
+			'errConf'              : 'Hibás backend konfiguráció.',
+			'errJSON'              : 'PHP JSON modul nincs telepítve.',
+			'errNoVolumes'         : 'Nem állnak rendelkezésre olvasható kötetek.',
+			'errCmdParams'         : 'érvénytelen paraméterek a parancsban. ("$1")',
 			'errDataNotJSON'       : 'A válasz nem JSON típusú adat.',
 			'errDataEmpty'         : 'Nem érkezett adat.',
-			'errCmdReq'            : 'Backend request requires command name.',
+			'errCmdReq'            : 'A backend kérelem parancsnevet igényel.',
 			'errOpen'              : '"$1" megnyitása nem sikerült.',
-			'errNotFolder'         : 'Object is not a folder.',
-			'errNotFile'           : 'Object is not a file.',
-			'errRead'              : 'Unable to read "$1".',
-			'errWrite'             : 'Unable to write into "$1".',
+			'errNotFolder'         : 'Az objektum nem egy mappa.',
+			'errNotFile'           : 'Az objektum nem egy fájl.',
+			'errRead'              : '"$1" olvasása nem sikerült.',
+			'errWrite'             : '"$1" írása nem sikerült.',
 			'errPerm'              : 'Engedély megtagadva.',
-			'errLocked'            : '"$1" is locked and can not be renamed, moved or removed.',
-			'errExists'            : 'File named "$1" already exists.',
-			'errInvName'           : 'Invalid file name.',
-			'errFolderNotFound'    : 'Folder not found.',
-			'errFileNotFound'      : 'File not found.',
-			'errTrgFolderNotFound' : 'Target folder "$1" not found.',
-			'errPopup'             : 'Browser prevented opening popup window. To open file enable it in browser options.',
-			'errMkdir'             : 'Unable to create folder "$1".',
-			'errMkfile'            : 'Unable to create file "$1".',
-			'errRename'            : 'Unable to rename "$1".',
-			'errCopyFrom'          : 'Copying files from volume "$1" not allowed.',
-			'errCopyTo'            : 'Copying files to volume "$1" not allowed.',
-			'errUpload'            : 'Feltöltési hiba.',
-			'errUploadFile'        : 'Nem sikerült a fájlt feltölteni. ($1)',
-			'errUploadNoFiles'     : 'No files found for upload.',
-			'errUploadTotalSize'   : 'Data exceeds the maximum allowed size.',
-			'errUploadFileSize'    : 'File exceeds maximum allowed size.',
-			'errUploadMime'        : 'File type not allowed.',
-			'errUploadTransfer'    : '"$1" transfer error.',
-			'errNotReplace'        : 'Object "$1" already exists at this location and can not be replaced by object with another type.',
-			'errReplace'           : 'Unable to replace "$1".',
+			'errLocked'            : '"$1" zárolás alatt van, és nem lehet átnevezni, mozgatni vagy eltávolítani.',
+			'errExists'            : '"$1" nevű fájl már létezik.',
+			'errInvName'           : 'Érvénytelen fáljnév.',
+			'errFolderNotFound'    : 'Mappa nem található.',
+			'errFileNotFound'      : 'Fájl nem található.',
+			'errTrgFolderNotFound' : 'Cél mappa nem található. ("$1")',
+			'errPopup'             : 'A böngésző megakadályozta egy felugró ablak megnyitását. A fájl megnyitását tegye lehetővé a böngésző beállitásaiban.',
+			'errMkdir'             : '"$1" mappa létrehozása sikertelen.',
+			'errMkfile'            : '"$1" fájl létrehozása sikertelen.',
+			'errRename'            : '"$1" átnevezése sikertelen.',
+			'errCopyFrom'          : 'Fájlok másolása a kötetről nem megengedett. ("$1")',
+			'errCopyTo'            : 'Fájlok másolása a kötetre nem megengedett. ("$1")',
+			'errMkOutLink'         : 'Hivatkozás létrehozása a root köteten kívül nem megengedett.', // from v2.1 added 03.10.2015
+			'errUpload'            : 'Feltöltési hiba.',  // old name - errUploadCommon
+			'errUploadFile'        : 'Nem sikerült a fájlt feltölteni. ($1)', // old name - errUpload
+			'errUploadNoFiles'     : 'Nem található fájl feltöltéshez.',
+			'errUploadTotalSize'   : 'Az adat meghaladja a maximálisan megengedett méretet.', // old name - errMaxSize
+			'errUploadFileSize'    : 'A fájl meghaladja a maximálisan megengedett méretet.', //  old name - errFileMaxSize
+			'errUploadMime'        : 'A fájltípus nem engedélyezett.',
+			'errUploadTransfer'    : '"$1" transzfer hiba.',
+			'errUploadTemp'        : 'Sikertelen az ideiglenes fájl léterhezozása feltöltéshez.', // from v2.1 added 26.09.2015
+			'errNotReplace'        : 'Az objektum "$1" már létezik ezen a helyen, és nem lehet cserélni másik típusra', // new
+			'errReplace'           : '"$1" nem cserélhető.',
 			'errSave'              : '"$1" mentése nem sikerült.',
 			'errCopy'              : '"$1" másolása nem sikerült.',
 			'errMove'              : '"$1" áthelyezése nem sikerült.',
 			'errCopyInItself'      : '"$1" nem másolható saját magára.',
 			'errRm'                : '"$1" törlése nem sikerült.',
-			'errRmSrc'             : 'Unable remove source file(s).',
+			'errRmSrc'             : 'Forrásfájl(ok) eltávolítása sikertelen.',
 			'errExtract'           : 'Unable to extract files from "$1".',
 			'errArchive'           : 'Unable to create archive.',
 			'errArcType'           : 'Nem támogatott archívum típus.',
@@ -91,7 +93,12 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'errFtpMkdir'          : 'Unable to create remote directory on FTP: "$1"',
 			'errArchiveExec'       : 'Error while archiving files: "$1"',
 			'errExtractExec'       : 'Error while extracting files: "$1"',
-			
+			'errNetUnMount'        : 'Unable to unmount', // from v2.1 added 30.04.2012
+			'errConvUTF8'          : 'Not convertible to UTF-8', // from v2.1 added 08.04.2014
+			'errFolderUpload'      : 'Try Google Chrome, If you\'d like to upload the folder.', // from v2.1 added 26.6.2015
+			'errSearchTimeout'     : 'Timed out while searching "$1". Search result is partial.', // from v2.1 added 12.1.2016
+			'errReauthRequire'     : 'Re-authorization is required.', // from v2.1.10 added 3.24.2016
+
 			/******************************* commands names ********************************/
 			'cmdarchive'   : 'Archívum létrehozása',
 			'cmdback'      : 'Vissza',
@@ -107,6 +114,7 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'cmdhome'      : 'Főkönyvtár',
 			'cmdinfo'      : 'Tulajdonságok',
 			'cmdmkdir'     : 'Új mappa',
+			'cmdmkdirin'   : 'Into new folder', // from v2.1.7 added 19.2.2016
 			'cmdmkfile'    : 'Új szöveges dokumentum',
 			'cmdopen'      : 'Megnyitás',
 			'cmdpaste'     : 'Beillesztés',
@@ -120,9 +128,14 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'cmdview'      : 'View',
 			'cmdresize'    : 'Resize & Rotate',
 			'cmdsort'      : 'Sort',
-			'cmdnetmount'  : 'Mount network volume',
-			
-			/*********************************** buttons ***********************************/ 
+			'cmdnetmount'  : 'Mount network volume', // added 18.04.2012
+			'cmdnetunmount': 'Unmount', // from v2.1 added 30.04.2012
+			'cmdplaces'    : 'To Places', // added 28.12.2014
+			'cmdchmod'     : 'Change mode', // from v2.1 added 20.6.2015
+			'cmdopendir'   : 'Open a folder', // from v2.1 added 13.1.2016
+			'cmdcolwidth'  : 'Reset column width', // from v2.1.13 added 12.06.2016
+
+			/*********************************** buttons ***********************************/
 			'btnClose'  : 'Bezár',
 			'btnSave'   : 'Ment',
 			'btnRm'     : 'Töröl',
@@ -130,8 +143,18 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'btnCancel' : 'Mégsem',
 			'btnNo'     : 'Nem',
 			'btnYes'    : 'Igen',
-			'btnMount'  : 'Mount',
-			
+			'btnMount'  : 'Mount',  // added 18.04.2012
+			'btnApprove': 'Goto $1 & approve', // from v2.1 added 26.04.2012
+			'btnUnmount': 'Unmount', // from v2.1 added 30.04.2012
+			'btnConv'   : 'Convert', // from v2.1 added 08.04.2014
+			'btnCwd'    : 'Here',      // from v2.1 added 22.5.2015
+			'btnVolume' : 'Volume',    // from v2.1 added 22.5.2015
+			'btnAll'    : 'All',       // from v2.1 added 22.5.2015
+			'btnMime'   : 'MIME Type', // from v2.1 added 22.5.2015
+			'btnFileName':'Filename',  // from v2.1 added 22.5.2015
+			'btnSaveClose': 'Save & Close', // from v2.1 added 12.6.2015
+			'btnBackup' : 'Backup', // fromv2.1 added 28.11.2015
+
 			/******************************** notifications ********************************/
 			'ntfopen'     : 'Mappa megnyitás',
 			'ntffile'     : 'Fájl megnyitás',
@@ -153,8 +176,14 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'ntfsmth'     : 'Doing something >_<',
 			'ntfloadimg'  : 'Loading image',
 			'ntfnetmount' : 'Mounting network volume', // added 18.04.2012
+			'ntfnetunmount': 'Unmounting network volume', // from v2.1 added 30.04.2012
 			'ntfdim'      : 'Acquiring image dimension', // added 20.05.2013
-			
+			'ntfreaddir'  : 'Reading folder infomation', // from v2.1 added 01.07.2013
+			'ntfurl'      : 'Getting URL of link', // from v2.1 added 11.03.2014
+			'ntfchmod'    : 'Changing file mode', // from v2.1 added 20.6.2015
+			'ntfpreupload': 'Verifying upload file name', // from v2.1 added 31.11.2015
+			'ntfzipdl'    : 'Creating a file for download', // from v2.1.7 added 23.1.2016
+
 			/************************************ dates **********************************/
 			'dateUnknown' : 'Ismeretlen',
 			'Today'       : 'Ma',
@@ -190,25 +219,36 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'Thursday'    : 'Thursday',
 			'Friday'      : 'Friday',
 			'Saturday'    : 'Saturday',
-			'Sun'         : 'Sun', 
-			'Mon'         : 'Mon', 
-			'Tue'         : 'Tue', 
-			'Wed'         : 'Wed', 
-			'Thu'         : 'Thu', 
-			'Fri'         : 'Fri', 
+			'Sun'         : 'Sun',
+			'Mon'         : 'Mon',
+			'Tue'         : 'Tue',
+			'Wed'         : 'Wed',
+			'Thu'         : 'Thu',
+			'Fri'         : 'Fri',
 			'Sat'         : 'Sat',
 
 			/******************************** sort variants ********************************/
-			'sortname'          : 'by name', 
-			'sortkind'          : 'by kind', 
+			'sortname'          : 'by name',
+			'sortkind'          : 'by kind',
 			'sortsize'          : 'by size',
 			'sortdate'          : 'by date',
 			'sortFoldersFirst'  : 'Folders first',
-			
+			'sortperm'          : 'by permission', // from v2.1.13 added 13.06.2016
+			'sortmode'          : 'by mode',       // from v2.1.13 added 13.06.2016
+			'sortowner'         : 'by owner',      // from v2.1.13 added 13.06.2016
+			'sortgroup'         : 'by group',      // from v2.1.13 added 13.06.2016
+
+			/********************************** new items **********************************/
+			'untitled file.txt' : 'NewFile.txt', // added 10.11.2015
+			'untitled folder'   : 'NewFolder',   // added 10.11.2015
+			'Archive'           : 'NewArchive',  // from v2.1 added 10.11.2015
+
 			/********************************** messages **********************************/
 			'confirmReq'      : 'Confirmation required',
 			'confirmRm'       : 'Valóban törölni akarja a kijelölt adatokat?<br/>Ez később nem fordítható vissza!',
 			'confirmRepl'     : 'Replace old file with new one?',
+			'confirmConvUTF8' : 'Not in UTF-8<br/>Convert to UTF-8?<br/>Contents become UTF-8 by saving after conversion.', // from v2.1 added 08.04.2014
+			'confirmNotSave'  : 'It has been modified.<br/>Losing work if you do not save changes.', // from v2.1 added 15.7.2015
 			'apllyAll'        : 'Apply to all',
 			'name'            : 'Név',
 			'size'            : 'Méret',
@@ -282,7 +322,38 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'port'                : 'Port', // added 18.04.2012
 			'user'                : 'User', // added 18.04.2012
 			'pass'                : 'Password', // added 18.04.2012
-			
+			'confirmUnmount'      : 'Are you unmount $1?',  // from v2.1 added 30.04.2012
+			'dropFilesBrowser': 'Drop or Paste files from browser', // from v2.1 added 30.05.2012
+			'dropPasteFiles'  : 'Drop or Paste files and URLs here', // from v2.1 added 07.04.2014
+			'encoding'        : 'Encoding', // from v2.1 added 19.12.2014
+			'locale'          : 'Locale',   // from v2.1 added 19.12.2014
+			'searchTarget'    : 'Target: $1',                // from v2.1 added 22.5.2015
+			'searchMime'      : 'Search by input MIME Type', // from v2.1 added 22.5.2015
+			'owner'           : 'Owner', // from v2.1 added 20.6.2015
+			'group'           : 'Group', // from v2.1 added 20.6.2015
+			'other'           : 'Other', // from v2.1 added 20.6.2015
+			'execute'         : 'Execute', // from v2.1 added 20.6.2015
+			'perm'            : 'Permission', // from v2.1 added 20.6.2015
+			'mode'            : 'Mode', // from v2.1 added 20.6.2015
+			'emptyFolder'     : 'Folder is empty', // from v2.1.6 added 30.12.2015
+			'emptyFolderDrop' : 'Folder is empty\\A Drop to add items', // from v2.1.6 added 30.12.2015
+			'emptyFolderLTap' : 'Folder is empty\\A Long tap to add items', // from v2.1.6 added 30.12.2015
+			'quality'         : 'Quality', // from v2.1.6 added 5.1.2016
+			'autoSync'        : 'Auto sync',  // from v2.1.6 added 10.1.2016
+			'moveUp'          : 'Move up',  // from v2.1.6 added 18.1.2016
+			'getLink'         : 'Get URL link', // from v2.1.7 added 9.2.2016
+			'selectedItems'   : 'Selected items ($1)', // from v2.1.7 added 2.19.2016
+			'folderId'        : 'Folder ID', // from v2.1.10 added 3.25.2016
+			'offlineAccess'   : 'Allow offline access', // from v2.1.10 added 3.25.2016
+			'reAuth'          : 'To re-authenticate', // from v2.1.10 added 3.25.2016
+			'nowLoading'      : 'Now loading...', // from v2.1.12 added 4.26.2016
+			'openMulti'       : 'Open multiple files', // from v2.1.12 added 5.14.2016
+			'openMultiConfirm': 'You are trying to open the $1 files. Are you sure you want to open in browser?', // from v2.1.12 added 5.14.2016
+			'emptySearch'     : 'Search results is empty', // from v2.1.12 added 5.16.2016
+			'editingFile'     : 'You are editing a file.', // from v2.1.13 added 6.3.2016
+			'hasSelected'     : 'You have selected $1 items.', // from v2.1.13 added 6.3.2016
+			'hasClipboard'    : 'You have $1 items in the clipboard.', // from v2.1.13 added 6.3.2016
+
 			/********************************** mimetypes **********************************/
 			'kindUnknown'     : 'Ismeretlen',
 			'kindFolder'      : 'Mappa',
@@ -332,6 +403,7 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'kindAWK'         : 'AWK forráskód',
 			'kindCSV'         : 'Comma separated values',
 			'kindDOCBOOK'     : 'Docbook XML dokumentum',
+			'kindMarkdown'    : 'Markdown text', // added 20.7.2015
 			// images
 			'kindImage'       : 'Kép',
 			'kindBMP'         : 'BMP kép',
@@ -364,3 +436,4 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 		}
 	};
 }
+
