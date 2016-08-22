4b49e2ee6362e76909aa88aef2ceb6291795142c
nao-pon <hypweb@gmail.com>
2016-08-22 11:37:34 +0900

    [cmd:toast] stop the removing sequence on mouseover

--- js/ui/toast.js
+++ js/ui/toast.js
@@ -23,34 +23,54 @@ $.fn.elfindertoast = function(opts, fm) {
 		extNode: undefined
 	};
 	return this.each(function() {
-		var self = $(this);
 		opts = $.extend({}, defOpts, opts || {});
+		
+		var self = $(this),
+			show = function(notm) {
+				self.stop();
+				self[opts.showMethod]({
+					duration: opts.showDuration,
+					easing: opts.showEasing,
+					complete: function() {
+						opts.onShown && opts.onShown();
+						if (!notm && opts.timeOut) {
+							rmTm = setTimeout(rm, opts.timeOut);
+						}
+					}
+				});
+			},
+			rm = function() {
+				self[opts.hideMethod]({
+					duration: opts.hideDuration,
+					easing: opts.hideEasing,
+					complete: function() {
+						opts.onHidden && opts.onHidden();
+						self.remove();
+					}
+				});
+			},
+			rmTm;
+		
 		self.on('click', function(e) {
 			e.stopPropagation();
 			e.preventDefault();
-			self.remove();
+			self.stop().remove();
+		}).on('mouseenter mouseleave', function(e) {
+			if (opts.timeOut) {
+				rmTm && clearTimeout(rmTm);
+				rmTm = null;
+				if (e.type === 'mouseenter') {
+					show(true);
+				} else {
+					rmTm = setTimeout(rm, opts.timeOut);
+				}
+			}
 		}).hide().addClass('toast-' + opts.mode).append($('<div class="elfinder-toast-msg"/>').html(opts.msg));
+		
 		if (opts.extNode) {
 			self.append(opts.extNode);
 		}
-		self[opts.showMethod]({
-			duration: opts.showDuration,
-			easing: opts.showEasing,
-			complete: function() {
-				opts.onShown && opts.onShown();
-				if (opts.timeOut) {
-					setTimeout(function() {
-						self[opts.hideMethod]({
-							duration: opts.hideDuration,
-							easing: opts.hideEasing,
-							complete: function() {
-								opts.onHidden && opts.onHidden();
-								self.remove();
-							}
-						});
-					}, opts.timeOut);
-				}
-			}
-		});
+		
+		show();
 	});
 };
