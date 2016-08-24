0e7ab69ebaded677fb106c250cf0766647c279ee
Naoki Sawada <hypweb@gmail.com>
2016-08-24 13:41:29 +0900

    Create i18nFolderMsgs.js

---
+++ i18nFolderMsgs.js
@@ -0,0 +1,26 @@
+var i18nFolderMsgs = {
+  'en': {
+    'volume_Demo': 'For Demo',
+    'folder_BasicAuthExample': 'Basic Auth Example',
+    'folder_Downloads': 'Downloads',
+    'folder_Images': 'Images',
+    'folder_LargeVideos': 'Videos',
+    'folder_MIME-types': 'MIME types',
+    'folder_Tips': 'Tips',
+    'folder_Welcome': 'Welcome(Multilingual)',
+    'folder_a_lot_sample': 'Plenty Items',
+    'folder_setup_with_composer': 'Setup with composer'
+  },
+  'jp': {
+    'volume_Demo': 'デモ用',
+    'folder_BasicAuthExample': 'Basic認証サンプル',
+    'folder_Downloads': 'ダウンロード',
+    'folder_Images': '画像',
+    'folder_LargeVideos': '動画',
+    'folder_MIME-types': 'MIMEタイプ',
+    'folder_Tips': 'ヒント',
+    'folder_Welcome': 'ようこそ(多言語)',
+    'folder_a_lot_sample': '多量サンプル',
+    'folder_setup_with_composer': 'composerでセットアップ'
+  }
+};
