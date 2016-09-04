bf07915c832961d729ed882bda611f485b153d25
nao-pon <hypweb@gmail.com>
2016-09-04 23:25:54 +0900

    src build elFinder-2.1-1442da9

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1.14 (2.1-src Nightly: af9de9d) (2016-09-04)
+ * Version 2.1.14 (2.1-src Nightly: 1442da9) (2016-09-04)
  * http://elfinder.org
  * 
  * Copyright 2009-2016, Studio 42
@@ -224,13 +224,6 @@ window.elFinder = function(node, opts) {
 		 **/
 		soundPath = './sounds/',
 				
-		/**
-		 * leaf roots cache
-		 * 
-		 * @type Object
-		 */
-		leafRoots = {},
-		
 		beeper = $(document.createElement('audio')).hide().appendTo('body')[0],
 			
 		syncInterval,
@@ -367,11 +360,11 @@ window.elFinder = function(node, opts) {
 					
 					// make or update of leaf roots cache
 					if (f.isroot && f.phash) {
-						if (! leafRoots[f.phash]) {
-							leafRoots[f.phash] = [ f.hash ];
+						if (! self.leafRoots[f.phash]) {
+							self.leafRoots[f.phash] = [ f.hash ];
 						} else {
-							if ($.inArray(f.hash, leafRoots[f.phash]) === -1) {
-								leafRoots[f.phash].push(f.hash);
+							if ($.inArray(f.hash, self.leafRoots[f.phash]) === -1) {
+								self.leafRoots[f.phash].push(f.hash);
 							}
 						}
 					}
@@ -1481,8 +1474,8 @@ window.elFinder = function(node, opts) {
 				
 				var resolve = function() {
 					var pushLeafRoots = function(name) {
-						if (leafRoots[data.target] && response[name]) {
-							$.each(leafRoots[data.target], function(i, h) {
+						if (self.leafRoots[data.target] && response[name]) {
+							$.each(self.leafRoots[data.target], function(i, h) {
 								var root;
 								if (root = self.file(h)) {
 									response[name].push(root);
@@ -2844,6 +2837,13 @@ window.elFinder = function(node, opts) {
 	 * @type Object
 	 */
 	this.roots = {};
+	
+	/**
+	 * leaf roots
+	 * 
+	 * @type Object
+	 */
+	this.leafRoots = {};
 	
 	/**
 	 * Loaded commands
@@ -5957,7 +5957,7 @@ if (!Object.keys) {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1.14 (2.1-src Nightly: af9de9d)';
+elFinder.prototype.version = '2.1.14 (2.1-src Nightly: 1442da9)';
 
 
 
@@ -19897,7 +19897,8 @@ elFinder.prototype.commands.search = function() {
 	 **/
 	this.exec = function(q, target, mime) {
 		var fm = this.fm,
-			reqDef;
+			reqDef = [],
+			phash;
 		
 		if (typeof q == 'string' && q) {
 			if (typeof target == 'object') {
@@ -19909,12 +19910,71 @@ elFinder.prototype.commands.search = function() {
 			$.each(mime, function(){ return $.trim(this); });
 			fm.trigger('searchstart', {query : q, target : target, mimes : mime});
 			
-			reqDef = fm.request({
-				data   : {cmd : 'search', q : q, target : target, mimes : mime},
-				notify : {type : 'search', cnt : 1, hideCnt : true},
-				cancel : true
+			if (target === '' && fm.api >= 2.1) {
+				$.each(fm.roots, function(id, hash) {
+					reqDef.push(fm.request({
+						data   : {cmd : 'search', q : q, target : hash, mimes : mime},
+						notify : {type : 'search', cnt : 1, hideCnt : (reqDef.length? false : true)},
+						cancel : true,
+						preventDone : true
+					}));
+				});
+			} else {
+				reqDef.push(fm.request({
+					data   : {cmd : 'search', q : q, target : target, mimes : mime},
+					notify : {type : 'search', cnt : 1, hideCnt : true},
+					cancel : true,
+					preventDone : true
+				}));
+				if (target !== '' && fm.api >= 2.1 && Object.keys(fm.leafRoots).length) {
+					$.each(fm.leafRoots, function(hash, roots) {
+						phash = hash;
+						while(phash) {
+							if (target === phash) {
+								$.each(roots, function() {
+									reqDef.push(fm.request({
+										data   : {cmd : 'search', q : q, target : this, mimes : mime},
+										notify : {type : 'search', cnt : 1, hideCnt : false},
+										cancel : true,
+										preventDone : true
+									}));
+								});
+							}
+							phash = (fm.file(phash) || {}).phash;
+						}
+					});
+				}
+			}
+			
+			return $.when.apply($, reqDef).done(function(data) {
+				var argLen = arguments.length,
+					i;
+				
+				data.warning && fm.error(data.warning);
+				
+				if (argLen > 1) {
+					data.files = (data.files || []);
+					for(i = 1; i < argLen; i++) {
+						arguments[i].warning && fm.error(arguments[i].warning);
+						
+						if (arguments[i].files) {
+							data.files.push.apply(data.files, arguments[i].files);
+						}
+					}
+				}
+				
+				fm.lazy(function() {
+					fm.trigger('search', data);
+				}).then(function() {
+					// fire event with command name + 'done'
+					return fm.lazy(function() {
+						fm.trigger('searchdone');
+					});
+				}).then(function() {
+					// force update content
+					data.sync && fm.sync();
+				});
 			});
-			return reqDef;
 		}
 		fm.getUI('toolbar').find('.'+fm.res('class', 'searchbtn')+' :text').focus();
 		return $.Deferred().reject();
