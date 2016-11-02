245091a61c3d3dd06f5ae306c1d3ae1cafab0c46
nao-pon <hypweb@gmail.com>
2016-11-02 23:01:05 +0900

    update demo elFinder-2.1-edb3543

--- demo/js/elfinder.full.js
+++ demo/js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1.16 (2.1-src Nightly: 57cfcbf) (2016-11-02)
+ * Version 2.1.16 (2.1-src Nightly: edb3543) (2016-11-02)
  * http://elfinder.org
  * 
  * Copyright 2009-2016, Studio 42
@@ -6317,7 +6317,7 @@ if (!Object.keys) {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1.16 (2.1-src Nightly: 57cfcbf)';
+elFinder.prototype.version = '2.1.16 (2.1-src Nightly: edb3543)';
 
 
 
@@ -6807,6 +6807,8 @@ elFinder.prototype._options = {
 			folders  : false,
 			// action after callback (""/"close"/"destroy")
 			oncomplete : '',
+			// get path before callback call
+			getPath    : true, 
 			// get image sizes before callback call
 			getImgSize : false
 		},
@@ -16814,46 +16816,50 @@ elFinder.prototype.commands.fullscreen = function() {
 			} else {
 				file.url = fm.url(file.hash);
 			}
-			file.path = fm.path(file.hash);
-			if (file.path === '' && file.phash) {
-				// get parents
-				(function() {
-					var dfd  = $.Deferred();
-					req.push(dfd);
-					fm.path(file.hash, false, {})
-						.done(function(path) {
-							file.path = path;
+			if (! opts.onlyURL) {
+				if (opts.getPath) {
+					file.path = fm.path(file.hash);
+					if (file.path === '' && file.phash) {
+						// get parents
+						(function() {
+							var dfd  = $.Deferred();
+							req.push(dfd);
+							fm.path(file.hash, false, {})
+								.done(function(path) {
+									file.path = path;
+								})
+								.fail(function() {
+									file.path = '';
+								})
+								.always(function() {
+									dfd.resolve();
+								});
+						})();
+					}
+				}
+				if (file.tmb && file.tmb != 1) {
+					file.tmb = tmb + file.tmb;
+				}
+				if (!file.width && !file.height) {
+					if (file.dim) {
+						dim = file.dim.split('x');
+						file.width = dim[0];
+						file.height = dim[1];
+					} else if (opts.getImgSize && file.mime.indexOf('image') !== -1) {
+						req.push(fm.request({
+							data : {cmd : 'dim', target : file.hash},
+							notify : {type : 'dim', cnt : 1, hideCnt : true},
+							preventDefault : true
 						})
-						.fail(function() {
-							file.path = '';
-						})
-						.always(function() {
-							dfd.resolve();
-						});
-				})();
-			}
-			if (file.tmb && file.tmb != 1) {
-				file.tmb = tmb + file.tmb;
-			}
-			if (!file.width && !file.height) {
-				if (file.dim) {
-					dim = file.dim.split('x');
-					file.width = dim[0];
-					file.height = dim[1];
-				} else if (opts.getImgSize && file.mime.indexOf('image') !== -1) {
-					req.push(fm.request({
-						data : {cmd : 'dim', target : file.hash},
-						notify : {type : 'dim', cnt : 1, hideCnt : true},
-						preventDefault : true
-					})
-					.done(function(data) {
-						if (data.dim) {
-							var dim = data.dim.split('x');
-							var rfile = fm.file(this.hash);
-							rfile.width = this.width = dim[0];
-							rfile.height = this.height = dim[1];
-						}
-					}.bind(file)));
+						.done(function(data) {
+							if (data.dim) {
+								var dim = data.dim.split('x');
+								var rfile = fm.file(this.hash);
+								rfile.width = this.width = dim[0];
+								rfile.height = this.height = dim[1];
+							}
+						}.bind(file)));
+					}
 				}
 			}
 		}
