ce3a8c6239fecc04ab6055718858b9fcbf19db8d
Naoki Sawada <hypweb@gmail.com>
2016-11-13 01:16:35 +0900

    Update elfinder.tr.js

--- js/i18n/elfinder.tr.js
+++ js/i18n/elfinder.tr.js
@@ -342,8 +342,8 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'perm'            : 'Yetki', // from v2.1 added 20.6.2015
 			'mode'            : 'Mod', // from v2.1 added 20.6.2015
 			'emptyFolder'     : 'Klasör boş', // from v2.1.6 added 30.12.2015
-			'emptyFolderDrop' : 'Klasör boş\\Eklemek için sürükleyin', // from v2.1.6 added 30.12.2015
-			'emptyFolderLTap' : 'Klasör boş\\Eklemek için basılı tutun', // from v2.1.6 added 30.12.2015
+			'emptyFolderDrop' : 'Klasör boş\\A Eklemek için sürükleyin', // from v2.1.6 added 30.12.2015
+			'emptyFolderLTap' : 'Klasör boş\\A Eklemek için basılı tutun', // from v2.1.6 added 30.12.2015
 			'quality'         : 'Kalite', // from v2.1.6 added 5.1.2016
 			'autoSync'        : 'Auto sync',  // from v2.1.6 added 10.1.2016
 			'moveUp'          : 'Move up',  // from v2.1.6 added 18.1.2016
