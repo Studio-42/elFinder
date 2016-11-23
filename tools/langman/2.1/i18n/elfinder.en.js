9ef893e82847bf2f2e6fe9452d09658e034781d6
nao-pon <hypweb@gmail.com>
2016-11-23 23:01:46 +0900

    langman i18n elFinder-2.1-23f6a46

--- tools/langman/2.1/i18n/elfinder.en.js
+++ tools/langman/2.1/i18n/elfinder.en.js
@@ -1,7 +1,7 @@
 /**
  * English translation
  * @author Troex Nevelin <troex@fury.scancode.ru>
- * @version 2016-11-13
+ * @version 2016-11-23
  */
 (function(root, factory) {
 	if (typeof define === 'function' && define.amd) {
@@ -380,7 +380,7 @@
 			'enabled'         : 'Enabled', // from v2.1.16 added 4.10.2016
 			'disabled'        : 'Disabled', // from v2.1.16 added 4.10.2016
 			'emptyIncSearch'  : 'Search results is empty in current view.\\APress [Enter] to expand search target.', // from v2.1.16 added 5.10.2016
-			'textLabel'       : 'Text lable', // from v2.1.17 added 13.10.2016
+			'textLabel'       : 'Text label', // from v2.1.17 added 13.10.2016
 			'minsLeft'        : '$1 mins left', // from v2.1.17 added 13.11.2016
 
 			/********************************** mimetypes **********************************/
