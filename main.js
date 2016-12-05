8e0453545de9f4ea70563d3354ac79e03b899f9d
Naoki Sawada <hypweb@gmail.com>
2016-12-05 18:10:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -103,7 +103,7 @@
 		paths : {
 			'jquery'   : '//cdnjs.cloudflare.com/ajax/libs/jquery/'+(ie8? '1.12.4' : jqver)+'/jquery.min',
 			'jquery-ui': '//cdnjs.cloudflare.com/ajax/libs/jqueryui/'+uiver+'/jquery-ui.min',
-			'elfinder' : 'elfinder.min',
+			'elfinder' : 'elfinder.full',
 			'elfinder.lang': [
 				'i18n/elfinder.'+lang,
 				'i18n/elfinder.fallback'
