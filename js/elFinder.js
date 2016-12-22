6c3ad7ecc9390d740d50191758767de700ac9e1b
Romaric Drigon <romaric@netinfluence.ch>
2016-12-22 09:44:43 +0100

    Added "sound" option

--- js/elFinder.js
+++ js/elFinder.js
@@ -2936,7 +2936,7 @@ var elFinder = function(node, opts) {
 				: self.trigger(name, $.isPlainObject(arg) ? arg : {});
 		}
 	});
-	
+
 	// bind core event handlers
 	this
 		.enable(function() {
@@ -3052,13 +3052,17 @@ var elFinder = function(node, opts) {
 			self.searchStatus.state = 0;
 			self.searchStatus.mixed = false;
 		})
-		.bind('rm', function(e) {
-			var play  = beeper.canPlayType && beeper.canPlayType('audio/wav; codecs="1"');
-		
-			play && play != '' && play != 'no' && $(beeper).html('<source src="' + soundPath + 'rm.wav" type="audio/wav">')[0].play()
-		})
-		
+
 		;
+
+	// We listen and emit a sound on delete according to option
+	if (true === this.options.sound) {
+		this.bind('rm', function(e) {
+            var play  = beeper.canPlayType && beeper.canPlayType('audio/wav; codecs="1"');
+
+            play && play != '' && play != 'no' && $(beeper).html('<source src="' + soundPath + 'rm.wav" type="audio/wav">')[0].play()
+        });
+	}
 
 	// bind external event handlers
 	$.each(this.options.handlers, function(event, callback) {
