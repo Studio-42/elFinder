99849a5c064586ef21a1c171de187bf1270fcf27
nao-pon <hypweb@gmail.com>
2017-04-23 23:06:09 +0900

    src build elFinder-2.1-4be2cc3

--- php/elFinderVolumeFTP.class.php
+++ php/elFinderVolumeFTP.class.php
@@ -49,6 +49,21 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 	protected $ftpOsUnix;
 	
 	/**
+	 * FTP LIST command option
+	 * 
+	 * @var string
+	 */
+	protected $ftpListOption = '-al';
+	
+	
+	/**
+	 * Is connected server Pure FTPd?
+	 * 
+	 * @var bool
+	 */
+	protected $isPureFtpd = false;
+	
+	/**
 	 * Tmp folder path
 	 *
 	 * @var string
@@ -91,8 +106,8 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 			'separator'     => '/',
 			'dirMode'       => 0755,
 			'fileMode'      => 0644,
-			'rootCssClass'  => 'elfinder-navbar-root-ftp'
-			
+			'rootCssClass'  => 'elfinder-navbar-root-ftp',
+			'ftpListOption' => '-al',
 		);
 		$this->options = array_merge($this->options, $opts); 
 		$this->options['mimeDetect'] = 'internal';
@@ -173,6 +188,10 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 		
 		if (is_null($this->options['syncChkAsTs'])) {
 			$this->options['syncChkAsTs'] = true;
+		}
+		
+		if(isset($this->options['ftpListOption'])) {
+			$this->ftpListOption = $this->options['ftpListOption'];
 		}
 		
 		return $this->connect();
@@ -269,7 +288,30 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 			}
 		}
 		
+		$help = ftp_raw($this->connect, 'HELP');
+		$this->isPureFtpd = stripos(implode(' ', $help), 'Pure-FTPd') !== false;
+		
 		return true;
+	}
+	
+	/**
+	 * Call ftp_rawlist with option prefix
+	 * 
+	 * @param string $path
+	 * @return array
+	 */
+	protected function ftpRawList($path) {
+		if ($this->isPureFtpd) {
+			$path = str_replace(' ', '\ ', $path);
+		}
+		if ($this->ftpListOption) {
+			$path = $this->ftpListOption . ' ' . $path;
+		}
+		$res = ftp_rawlist($this->connect, $path);
+		if ($res === false) {
+			$res = array();
+		}
+		return $res;
 	}
 	
 	/*********************************************************************/
@@ -439,7 +481,7 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 
 		$list = array();
 		$encPath = $this->convEncIn($path);
-		foreach (ftp_rawlist($this->connect, $encPath) as $raw) {
+		foreach ($this->ftpRawList($encPath) as $raw) {
 			if (($stat = $this->parseRaw($raw, $encPath))) {
 				$list[] = $stat;
 			}
@@ -707,7 +749,7 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 				);
 				if ($this->isMyReload()) {
 					$ts = 0;
-					foreach (ftp_rawlist($this->connect, $path) as $str) {
+					foreach ($this->ftpRawList($path) as $str) {
 						if (($stat = $this->parseRaw($str, $path))) {
 							$ts = max($ts, $stat['ts']);
 						}
@@ -751,6 +793,7 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 			array_pop($parts);
 			$parts = array_map('strtolower', $parts);
 			$stat  = array();
+			$mode  = '';
 			foreach ($parts as $part) {
 
 				list($key, $val) = explode('=', $part);
@@ -770,7 +813,15 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 						break;
 
 					case 'unix.mode':
-						$stat['chmod'] = $val;
+						$mode = strval($val);
+						break;
+
+					case 'unix.uid':
+						$stat['owner'] = $val;
+						break;
+
+					case 'unix.gid':
+						$stat['group'] = $val;
 						break;
 
 					case 'perm':
@@ -790,15 +841,16 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 				$stat['size'] = 0;
 			}
 			
-			if (isset($stat['chmod'])) {
+			if ($mode) {
 				$stat['perm'] = '';
-				if ($stat['chmod'][0] == 0) {
-					$stat['chmod'] = substr($stat['chmod'], 1);
+				if ($mode[0] === '0') {
+					$mode = substr($mode, 1);
 				}
 
+				$perm = array();
 				for ($i = 0; $i <= 2; $i++) {
 					$perm[$i] = array(false, false, false);
-					$n = isset($stat['chmod'][$i]) ? $stat['chmod'][$i] : 0;
+					$n = isset($mode[$i]) ? $mode[$i] : 0;
 					
 					if ($n - 4 >= 0) {
 						$perm[$i][0] = true;
@@ -822,8 +874,6 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 					} else {
 						$stat['perm'] .= '-';
 					}
-					
-					$stat['perm'] .= ' ';
 				}
 				
 				$stat['perm'] = trim($stat['perm']);
@@ -833,8 +883,12 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 
 				$stat['read']  = $stat['mime'] == 'directory' ? $read && (($owner && $perm[0][2]) || $perm[1][2] || $perm[2][2]) : $read;
 				$stat['write'] = ($owner && $perm[0][1]) || $perm[1][1] || $perm[2][1];
-				unset($stat['chmod']);
 
+				if ($this->options['statOwner']) {
+					$stat['isowner'] = $owner;
+				} else {
+					unset($stat['owner'], $stat['group'], $stat['perm']);
+				}
 			}
 			
 			return $stat;
@@ -853,7 +907,7 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 	 **/
 	protected function _subdirs($path) {
 		
-		foreach (ftp_rawlist($this->connect, $path) as $str) {
+		foreach ($this->ftpRawList($path) as $str) {
 			$info = preg_split('/\s+/', $str, 9);
 			if (!isset($this->ftpOsUnix)) {
 				$this->ftpOsUnix = !preg_match('/\d/', substr($info[0], 0, 1));
@@ -899,7 +953,7 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 	protected function _scandir($path) {
 		$files = array();
 
- 		foreach (ftp_rawlist($this->connect, $path) as $str) {
+ 		foreach ($this->ftpRawList($path) as $str) {
  			if (($stat = $this->parseRaw($str, $path, true))) {
  				$files[] = $this->_joinPath($path, $stat['name']);
  			}
@@ -1410,7 +1464,7 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 	 */
 	protected function ftp_scan_dir($remote_directory, $targets = null)
 	{
-		$buff = ftp_rawlist($this->connect, $remote_directory);
+		$buff = $this->ftpRawList($remote_directory);
 		$items = array();
 		if ($targets && is_array($targets)) {
 			$targets = array_flip($targets);
