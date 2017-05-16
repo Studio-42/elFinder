d0355f10572467313fa16854b47779e097ec2c3f
Naoki Sawada <hypweb@gmail.com>
2017-05-16 18:06:06 +0900

    Update setup_with_composer.sh

--- tools/installer/setup_with_composer/setup_with_composer.sh
+++ tools/installer/setup_with_composer/setup_with_composer.sh
@@ -87,6 +87,51 @@ if ($useFtpNetMount) {
 	elFinder::$netDrivers['ftp'] = 'FTP';
 }
 
+// // Required for Dropbox network mount
+// // Installation by composer
+// // `composer require kunalvarma05/dropbox-php-sdk`
+// // Enable network mount
+// elFinder::$netDrivers['dropbox2'] = 'Dropbox2';
+// // Dropbox2 Netmount driver need next two settings. You can get at https://www.dropbox.com/developers/apps
+// // AND reuire regist redirect url to "YOUR_CONNECTOR_URL?cmd=netmount&protocol=dropbox2&host=1"
+// define('ELFINDER_DROPBOX_APPKEY',    '');
+// define('ELFINDER_DROPBOX_APPSECRET', '');
+// ===============================================
+
+// // Required for Google Drive network mount
+// // Installation by composer
+// // `composer require google/apiclient:^2.0`
+// // Enable network mount
+// elFinder::$netDrivers['googledrive'] = 'GoogleDrive';
+// // GoogleDrive Netmount driver need next two settings. You can get at https://console.developers.google.com
+// // AND reuire regist redirect url to "YOUR_CONNECTOR_URL?cmd=netmount&protocol=googledrive&host=1"
+// define('ELFINDER_GOOGLEDRIVE_CLIENTID',     '');
+// define('ELFINDER_GOOGLEDRIVE_CLIENTSECRET', '');
+// // Required case of without composer
+// define('ELFINDER_GOOGLEDRIVE_GOOGLEAPICLIENT', '/path/to/google-api-php-client/vendor/autoload.php');
+// ===============================================
+
+// // Required for One Drive network mount
+// //  * cURL PHP extension required
+// //  * HTTP server PATH_INFO supports required
+// // Enable network mount
+// elFinder::$netDrivers['onedrive'] = 'OneDrive';
+// // GoogleDrive Netmount driver need next two settings. You can get at https://dev.onedrive.com
+// // AND reuire regist redirect url to "YOUR_CONNECTOR_URL/netmount/onedrive/1"
+// define('ELFINDER_ONEDRIVE_CLIENTID',     '');
+// define('ELFINDER_ONEDRIVE_CLIENTSECRET', '');
+// ===============================================
+
+// // Required for Box network mount
+// //  * cURL PHP extension required
+// // Enable network mount
+// elFinder::$netDrivers['box'] = 'Box';
+// // Box Netmount driver need next two settings. You can get at https://developer.box.com
+// // AND reuire regist redirect url to "YOUR_CONNECTOR_URL"
+// define('ELFINDER_BOX_CLIENTID',     '');
+// define('ELFINDER_BOX_CLIENTSECRET', '');
+// ===============================================
+
 /**
  * Simple function to demonstrate how to control file access using "accessControl" callback.
  * This method will disable accessing files/folders starting from '.' (dot)
