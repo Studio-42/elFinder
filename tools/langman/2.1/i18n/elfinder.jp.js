47e17f7e20a9be70fd20b1b79fdafcdd16d80667
nao-pon <hypweb@gmail.com>
2017-08-18 17:09:49 +0900

    langman i18n elFinder-2.1-7733894

--- tools/langman/2.1/i18n/elfinder.jp.js
+++ tools/langman/2.1/i18n/elfinder.jp.js
@@ -43,7 +43,7 @@
 			'errDataEmpty'         : '空のデータです。',
 			'errCmdReq'            : 'バックエンドリクエストはコマンド名が必要です。',
 			'errOpen'              : '"$1" を開くことができません。',
-			'errNotFolder'         : 'オブジェクトがフォルダーではありません。',
+			'errNotFolder'         : 'オブジェクトがフォルダではありません。',
 			'errNotFile'           : 'オブジェクトがファイルではありません。',
 			'errRead'              : '"$1" を読み込むことができません。',
 			'errWrite'             : '"$1" に書き込むことができません。',
@@ -52,11 +52,11 @@
 			'errExists'            : '"$1" というアイテム名はすでに存在しています。',
 			'errInvName'           : '無効なファイル名です。',
 			'errInvDirname'        : '無効なフォルダ名です。',  // from v2.1.24 added 12.4.2017
-			'errFolderNotFound'    : 'フォルダーが見つかりません。',
+			'errFolderNotFound'    : 'フォルダが見つかりません。',
 			'errFileNotFound'      : 'ファイルが見つかりません。',
-			'errTrgFolderNotFound' : 'ターゲットとするフォルダー "$1" が見つかりません。',
+			'errTrgFolderNotFound' : 'ターゲットとするフォルダ "$1" が見つかりません。',
 			'errPopup'             : 'ポップアップウィンドウが開けません。ファイルを開くにはブラウザの設定を変更してください。',
-			'errMkdir'             : 'フォルダー "$1" を作成することができません。',
+			'errMkdir'             : 'フォルダ "$1" を作成することができません。',
 			'errMkfile'            : 'ファイル "$1" を作成することができません。',
 			'errRename'            : '"$1" の名前を変更することができません。',
 			'errCopyFrom'          : '"$1" からのファイルコピーは許可されていません。',
@@ -114,7 +114,7 @@
 			'errRestore'           : '宛先の特定ができないため、ごみ箱から戻せません。', // from v2.1.24 added 3.5.2017
 			'errEditorNotFound'    : 'このファイルタイプのエディターがありません。', // from v2.1.25 added 23.5.2017
 			'errServerError'       : 'サーバー側でエラーが発生しました。', // from v2.1.25 added 16.6.2017
-			'errEmpty'             : 'フォルダー"$1"を空にすることができません。', // from v2.1.25 added 22.6.2017
+			'errEmpty'             : 'フォルダ"$1"を空にすることができません。', // from v2.1.25 added 22.6.2017
 			
 			/******************************* commands names ********************************/
 			'cmdarchive'   : 'アーカイブ作成',
@@ -130,8 +130,8 @@
 			'cmdhelp'      : 'このソフトウェアについて',
 			'cmdhome'      : 'ルート',
 			'cmdinfo'      : '情報',
-			'cmdmkdir'     : '新規フォルダー',
-			'cmdmkdirin'   : '新規フォルダーへ', // from v2.1.7 added 19.2.2016
+			'cmdmkdir'     : '新規フォルダ',
+			'cmdmkdirin'   : '新規フォルダへ', // from v2.1.7 added 19.2.2016
 			'cmdmkfile'    : '新規テキストファイル',
 			'cmdopen'      : '開く',
 			'cmdpaste'     : 'ペースト',
@@ -142,7 +142,7 @@
 			'cmdtrash'     : 'ごみ箱へ', //from v2.1.24 added 29.4.2017
 			'cmdrestore'   : '復元', //from v2.1.24 added 3.5.2017
 			'cmdsearch'    : 'ファイルを探す',
-			'cmdup'        : '親ディレクトリーへ移動',
+			'cmdup'        : '親フォルダへ移動',
 			'cmdupload'    : 'ファイルアップロード',
 			'cmdview'      : 'ビュー',
 			'cmdresize'    : 'リサイズと回転',
@@ -155,7 +155,7 @@
 			'cmdcolwidth'  : '列幅リセット', // from v2.1.13 added 12.06.2016
 			'cmdfullscreen': 'フルスクリーン', // from v2.1.15 added 03.08.2016
 			'cmdmove'      : '移動', // from v2.1.15 added 21.08.2016
-			'cmdempty'     : 'フォルダーを空に', // from v2.1.25 added 22.06.2017
+			'cmdempty'     : 'フォルダを空に', // from v2.1.25 added 22.06.2017
 			'cmdundo'      : '元に戻す', // from v2.1.27 added 31.07.2017
 			'cmdredo'      : 'やり直し', // from v2.1.27 added 31.07.2017
 			'cmdpreference': '環境設定', // from v2.1.27 added 03.08.2017
@@ -189,10 +189,10 @@
 			'btnSaveAs'   : '別名保存', // from v2.1.25 added 24.5.2017
 
 			/******************************** notifications ********************************/
-			'ntfopen'     : 'フォルダーを開いています',
+			'ntfopen'     : 'フォルダを開いています',
 			'ntffile'     : 'ファイルを開いています',
-			'ntfreload'   : 'フォルダーを再読込しています',
-			'ntfmkdir'    : 'フォルダーを作成しています',
+			'ntfreload'   : 'フォルダを再読込しています',
+			'ntfmkdir'    : 'フォルダを作成しています',
 			'ntfmkfile'   : 'ファイルを作成しています',
 			'ntfrm'       : 'ファイルを削除しています',
 			'ntfcopy'     : 'ファイルをコピーしています',
@@ -320,7 +320,7 @@
 			'locked'          : 'ロック',
 			'dim'             : 'サイズ',
 			'files'           : 'ファイル',
-			'folders'         : 'フォルダー',
+			'folders'         : 'フォルダ',
 			'items'           : 'アイテム',
 			'yes'             : 'はい',
 			'no'              : 'いいえ',
@@ -419,13 +419,13 @@
 			'minsLeft'        : '残り$1分', // from v2.1.17 added 13.11.2016
 			'openAsEncoding'  : '選択したエンコーディングで開き直す', // from v2.1.19 added 2.12.2016
 			'saveAsEncoding'  : '選択したエンコーディングで保存', // from v2.1.19 added 2.12.2016
-			'selectFolder'    : 'フォルダーを選択', // from v2.1.20 added 13.12.2016
+			'selectFolder'    : 'フォルダを選択', // from v2.1.20 added 13.12.2016
 			'firstLetterSearch': '一文字目で検索', // from v2.1.23 added 24.3.2017
 			'presets'         : 'プリセット', // from v2.1.25 added 26.5.2017
 			'tooManyToTrash'  : 'アイテム数が多すぎるのでごみ箱に入れられません。', // from v2.1.25 added 9.6.2017
 			'TextArea'        : 'テキストエリア', // from v2.1.25 added 14.6.2017
-			'folderToEmpty'   : 'フォルダー"$1"を空にします。', // from v2.1.25 added 22.6.2017
-			'filderIsEmpty'   : 'フォルダー"$1"にアイテムはありません。', // from v2.1.25 added 22.6.2017
+			'folderToEmpty'   : 'フォルダ"$1"を空にします。', // from v2.1.25 added 22.6.2017
+			'filderIsEmpty'   : 'フォルダ"$1"にアイテムはありません。', // from v2.1.25 added 22.6.2017
 			'preference'      : '環境設定', // from v2.1.26 added 28.6.2017
 			'language'        : '言語設定', // from v2.1.26 added 28.6.2017
 			'clearBrowserData': 'ブラウザに保存された設定を初期化する', // from v2.1.26 added 28.6.2017
@@ -434,7 +434,7 @@
 			/********************************** mimetypes **********************************/
 			'kindUnknown'     : '不明',
 			'kindRoot'        : 'ボリュームルート', // from v2.1.16 added 16.10.2016
-			'kindFolder'      : 'フォルダー',
+			'kindFolder'      : 'フォルダ',
 			'kindAlias'       : '別名',
 			'kindAliasBroken' : '宛先不明の別名',
 			// applications
