d38bc4fa47a17f375c4766af6205368aa5ed9785
nao-pon <hypweb@gmail.com>
2017-11-21 16:23:47 +0900

    [VD:OneDrive] fix problem of not show thumbnail of new items

--- php/elFinderVolumeOneDrive.class.php
+++ php/elFinderVolumeOneDrive.class.php
@@ -436,6 +436,8 @@ class elFinderVolumeOneDrive extends elFinderVolumeDriver
                 if ($raw->thumbnails[0]->small->url) {
                     $stat['tmb'] = substr($raw->thumbnails[0]->small->url, 8); // remove "https://"
                 }
+            } else if ($raw->file->processingMetadata) {
+                $stat['tmb'] = '1';
             }
         }
 
