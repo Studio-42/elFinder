35c5e424d9d50ff2e790108ed3cdfb0dfeb63ca5
Naoki Sawada <hypweb@gmail.com>
2017-11-22 09:22:29 +0900

    [VD:FTP] CS fixes

--- php/elFinderVolumeFTP.class.php
+++ php/elFinderVolumeFTP.class.php
@@ -244,10 +244,9 @@ class elFinderVolumeFTP extends elFinderVolumeDriver {
 	 * @author Dmitry (dio) Levashov
 	 **/
 	protected function connect() {
-		if ($this->options['ssl']==true && !($this->connect = ftp_ssl_connect($this->options['host'], $this->options['port'], $this->options['timeout']))) {
+		if (!empty($this->options['ssl']) && !($this->connect = ftp_ssl_connect($this->options['host'], $this->options['port'], $this->options['timeout']))) {
 			return $this->setError('Unable to connect to FTP server '.$this->options['host'].' with SSL');
-		}
-		elseif (!($this->connect = ftp_connect($this->options['host'], $this->options['port'], $this->options['timeout']))) {
+		} else if (!($this->connect = ftp_connect($this->options['host'], $this->options['port'], $this->options['timeout']))) {
 			return $this->setError('Unable to connect to FTP server '.$this->options['host']);
 		}
 		if (!ftp_login($this->connect, $this->options['user'], $this->options['pass'])) {
