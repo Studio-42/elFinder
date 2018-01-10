f68bcbf0aaa4d2813ca7451453e7f51f91066081
nao-pon <hypweb@gmail.com>
2018-01-10 14:18:39 +0900

    src build elFinder-2.1-49776fd

--- php/elFinderVolumeDriver.class.php
+++ php/elFinderVolumeDriver.class.php
@@ -292,7 +292,8 @@ abstract class elFinderVolumeDriver {
 			'webp:application/octet-stream'=> 'image/webp',
 			'doc:application/vnd.ms-office'=> 'application/msword',
 			'xls:application/vnd.ms-office'=> 'application/vnd.ms-excel',
-			'ppt:application/vnd.ms-office'=> 'application/vnd.ms-powerpoint'
+			'ppt:application/vnd.ms-office'=> 'application/vnd.ms-powerpoint',
+			'yml:text/plain'               => 'text/x-yaml'
 		),
 		// An option to add MimeMap to the `mimeMap` option
 		// Array '[ext]:[detected mime type]' => '[normalized mime]'
@@ -535,131 +536,45 @@ abstract class elFinderVolumeDriver {
 	protected $disabled = array();
 	
 	/**
-	 * default extensions/mimetypes for mimeDetect == 'internal' 
+	 * overwrite extensions/mimetypes to mime.types
 	 *
 	 * @var array
 	 **/
 	protected static $mimetypes = array(
 		// applications
-		'ai'    => 'application/postscript',
-		'eps'   => 'application/postscript',
 		'exe'   => 'application/x-executable',
-		'doc'   => 'application/msword',
-		'dot'   => 'application/msword',
-		'xls'   => 'application/vnd.ms-excel',
-		'xlt'   => 'application/vnd.ms-excel',
-		'xla'   => 'application/vnd.ms-excel',
-		'ppt'   => 'application/vnd.ms-powerpoint',
-		'pps'   => 'application/vnd.ms-powerpoint',
-		'pdf'   => 'application/pdf',
-		'xml'   => 'application/xml',
-		'swf'   => 'application/x-shockwave-flash',
-		'torrent' => 'application/x-bittorrent',
 		'jar'   => 'application/x-jar',
-		// open office (finfo detect as application/zip)
-		'odt'   => 'application/vnd.oasis.opendocument.text',
-		'ott'   => 'application/vnd.oasis.opendocument.text-template',
-		'oth'   => 'application/vnd.oasis.opendocument.text-web',
-		'odm'   => 'application/vnd.oasis.opendocument.text-master',
-		'odg'   => 'application/vnd.oasis.opendocument.graphics',
-		'otg'   => 'application/vnd.oasis.opendocument.graphics-template',
-		'odp'   => 'application/vnd.oasis.opendocument.presentation',
-		'otp'   => 'application/vnd.oasis.opendocument.presentation-template',
-		'ods'   => 'application/vnd.oasis.opendocument.spreadsheet',
-		'ots'   => 'application/vnd.oasis.opendocument.spreadsheet-template',
-		'odc'   => 'application/vnd.oasis.opendocument.chart',
-		'odf'   => 'application/vnd.oasis.opendocument.formula',
-		'odb'   => 'application/vnd.oasis.opendocument.database',
-		'odi'   => 'application/vnd.oasis.opendocument.image',
-		'oxt'   => 'application/vnd.openofficeorg.extension',
-		// MS office 2007 (finfo detect as application/zip)
-		'docx'  => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-		'docm'  => 'application/vnd.ms-word.document.macroEnabled.12',
-		'dotx'  => 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
-		'dotm'  => 'application/vnd.ms-word.template.macroEnabled.12',
-		'xlsx'  => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-		'xlsm'  => 'application/vnd.ms-excel.sheet.macroEnabled.12',
-		'xltx'  => 'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
-		'xltm'  => 'application/vnd.ms-excel.template.macroEnabled.12',
-		'xlsb'  => 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
-		'xlam'  => 'application/vnd.ms-excel.addin.macroEnabled.12',
-		'pptx'  => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-		'pptm'  => 'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
-		'ppsx'  => 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
-		'ppsm'  => 'application/vnd.ms-powerpoint.slideshow.macroEnabled.12',
-		'potx'  => 'application/vnd.openxmlformats-officedocument.presentationml.template',
-		'potm'  => 'application/vnd.ms-powerpoint.template.macroEnabled.12',
-		'ppam'  => 'application/vnd.ms-powerpoint.addin.macroEnabled.12',
-		'sldx'  => 'application/vnd.openxmlformats-officedocument.presentationml.slide',
-		'sldm'  => 'application/vnd.ms-powerpoint.slide.macroEnabled.12',
 		// archives
 		'gz'    => 'application/x-gzip',
 		'tgz'   => 'application/x-gzip',
-		'bz'    => 'application/x-bzip2',
-		'bz2'   => 'application/x-bzip2',
 		'tbz'   => 'application/x-bzip2',
-		'xz'    => 'application/x-xz',
-		'zip'   => 'application/zip',
 		'rar'   => 'application/x-rar',
-		'tar'   => 'application/x-tar',
-		'7z'    => 'application/x-7z-compressed',
 		// texts
-		'txt'   => 'text/plain',
 		'php'   => 'text/x-php',
-		'html'  => 'text/html',
-		'htm'   => 'text/html',
 		'js'    => 'text/javascript',
-		'css'   => 'text/css',
-		'rtf'   => 'application/rtf',
 		'rtfd'  => 'application/rtfd',
 		'py'    => 'text/x-python',
-		'java'  => 'text/x-java-source',
 		'rb'    => 'text/x-ruby',
 		'sh'    => 'text/x-shellscript',
 		'pl'    => 'text/x-perl',
 		'xml'   => 'text/xml',
-		'sql'   => 'text/x-sql',
 		'c'     => 'text/x-csrc',
 		'h'     => 'text/x-chdr',
 		'cpp'   => 'text/x-c++src',
 		'hh'    => 'text/x-c++hdr',
-		'log'   => 'text/plain',
-		'csv'   => 'text/csv',
 		'md'    => 'text/x-markdown',
 		'markdown' => 'text/x-markdown',
+		'yml'   => 'text/x-yaml',
 		// images
 		'bmp'   => 'image/x-ms-bmp',
-		'jpg'   => 'image/jpeg',
-		'jpeg'  => 'image/jpeg',
-		'gif'   => 'image/gif',
-		'png'   => 'image/png',
-		'tif'   => 'image/tiff',
-		'tiff'  => 'image/tiff',
 		'tga'   => 'image/x-targa',
-		'psd'   => 'image/vnd.adobe.photoshop',
-		//'ai'    => 'image/vnd.adobe.photoshop',
 		'xbm'   => 'image/xbm',
 		'pxm'   => 'image/pxm',
 		//audio
-		'mp3'   => 'audio/mpeg',
-		'mid'   => 'audio/midi',
-		'ogg'   => 'audio/ogg',
-		'oga'   => 'audio/ogg',
-		'm4a'   => 'audio/mp4',
 		'wav'   => 'audio/wav',
-		'wma'   => 'audio/x-ms-wma',
 		// video
-		'avi'   => 'video/x-msvideo',
 		'dv'    => 'video/x-dv',
-		'mp4'   => 'video/mp4',
-		'mpeg'  => 'video/mpeg',
-		'mpg'   => 'video/mpeg',
-		'mov'   => 'video/quicktime',
 		'wm'    => 'video/x-ms-wmv',
-		'flv'   => 'video/x-flv',
-		'mkv'   => 'video/x-matroska',
-		'webm'  => 'video/webm',
-		'ogv'   => 'video/ogg',
 		'ogm'   => 'video/ogg',
 		'm2ts'  => 'video/MP2T',
 		'mts'   => 'video/MP2T',
@@ -1259,7 +1174,7 @@ abstract class elFinderVolumeDriver {
 		// load mimes from external file for mimeDetect == 'internal'
 		// based on Alexey Sukhotin idea and patch: http://elrte.org/redmine/issues/163
 		// file must be in file directory or in parent one 
-		if ($this->mimeDetect == 'internal' && !self::$mimetypesLoaded) {
+		if ($this->mimeDetect === 'internal' && !self::$mimetypesLoaded) {
 			self::$mimetypesLoaded = true;
 			$this->mimeDetect = 'internal';
 			$file = false;
@@ -4322,7 +4237,8 @@ abstract class elFinderVolumeDriver {
 			$nameCheck = true;
 		}
 		$ext = (false === $pos = strrpos($name, '.')) ? '' : substr($name, $pos + 1);
-		if (! $nameCheck && is_readable($path) && filesize($path) > 0) {
+		$size = filesize($path);
+		if (! $nameCheck && is_readable($path) && $size > 0) {
 			// detecting by contents
 			if ($this->mimeDetect === 'finfo') {
 				$type = finfo_file($this->finfo, $path);
@@ -4342,6 +4258,9 @@ abstract class elFinderVolumeDriver {
 		if (! $type) {
 			// detecting by filename
 			$type = elFinderVolumeDriver::mimetypeInternalDetect($name);
+			if ($type === 'unknown' && $size === 0) {
+				$type = 'text/plain';
+			}
 		}
 		
 		$type = explode(';', $type);
@@ -4376,24 +4295,7 @@ abstract class elFinderVolumeDriver {
 	static protected function mimetypeInternalDetect($path = '') {
 		// load default MIME table file "mime.types"
 		if (!elFinderVolumeDriver::$mimetypesLoaded) {
-			elFinderVolumeDriver::$mimetypesLoaded = true;
-			$file = elFinder::$defaultMimefile;
-			if ($file === '' || ! is_readable($file)) {
-				$file = dirname(__FILE__).DIRECTORY_SEPARATOR.'mime.types';
-			}
-			if (is_readable($file)) {
-				$mimecf = file($file);
-				foreach ($mimecf as $line_num => $line) {
-					if (!preg_match('/^\s*#/', $line)) {
-						$mime = preg_split('/\s+/', $line, -1, PREG_SPLIT_NO_EMPTY);
-						for ($i = 1, $size = count($mime); $i < $size ; $i++) {
-							if (!isset(elFinderVolumeDriver::$mimetypes[$mime[$i]])) {
-								elFinderVolumeDriver::$mimetypes[$mime[$i]] = $mime[0];
-							}
-						}
-					}
-				}
-			}
+			$this->getMimeTable();
 		}
 		$ext = '';
 		if ($path) {
