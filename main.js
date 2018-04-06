2df5a92b6b5d65761d57433c4fe8fa2c763dd40e
Naoki Sawada <hypweb+github@gmail.com>
2018-04-06 22:26:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -43,6 +43,9 @@
 				},
 				quicklook : {
 					googleDocsMimes : ['application/pdf', 'image/tiff', 'application/vnd.ms-office', 'application/msword', 'application/vnd.ms-word', 'application/vnd.ms-excel', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation']
+				},
+				opennew : {
+					url : 'fullscreen.html'
 				}
 			},
 			lang: lang
