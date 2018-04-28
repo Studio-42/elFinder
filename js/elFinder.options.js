5b9a8e5cc038fc4c055c52daf14b6f9ae3be96fd
nao-pon <hypweb@gmail.com>
2018-04-28 21:09:53 +0900

    [js:options] update cdns

--- js/elFinder.options.js
+++ js/elFinder.options.js
@@ -13,9 +13,9 @@ elFinder.prototype._options = {
 	cdns : {
 		// for editor etc.
 		ace        : '//cdnjs.cloudflare.com/ajax/libs/ace/1.3.3',
-		codemirror : '//cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0',
+		codemirror : '//cdnjs.cloudflare.com/ajax/libs/codemirror/5.37.0',
 		ckeditor   : '//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.9.2',
-		ckeditor5  : '//cdn.ckeditor.com/ckeditor5/1.0.0-beta.3',
+		ckeditor5  : '//cdn.ckeditor.com/ckeditor5/10.0.0',
 		tinymce    : '//cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.11',
 		simplemde  : '//cdnjs.cloudflare.com/ajax/libs/simplemde/1.11.2',
 		// for quicklook etc.
