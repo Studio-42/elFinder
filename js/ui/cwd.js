556a9d26ce5127a33672b65154d31437c394853b
nao-pon <hypweb@gmail.com>
2018-06-01 01:05:00 +0900

    [ui:cwd] remove debugging code

--- js/ui/cwd.js
+++ js/ui/cwd.js
@@ -1926,7 +1926,6 @@ $.fn.elfindercwd = function(fm, options) {
 							} else {
 								if (!$this.hasClass(clSelected)) {
 									if (list) {
-										fm.log($(e.target));
 										disable = $(e.target).closest('span,tr').is('tr');
 									} else {
 										disable = $(e.target).hasClass('elfinder-cwd-file');
