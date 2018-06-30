9dcedafd27cb82576d8424a8e9617a1697429c38
nao-pon <hypweb@gmail.com>
2018-07-01 00:28:15 +0900

    langman i18n elFinder-2.1-66d4f39

--- tools/langman/2.1/i18n/elfinder.en.js
+++ tools/langman/2.1/i18n/elfinder.en.js
@@ -2,7 +2,7 @@
  * English translation
  * @author Troex Nevelin <troex@fury.scancode.ru>
  * @author Naoki Sawada <hypweb+elfinder@gmail.com>
- * @version 2018-05-22
+ * @version 2018-06-30
  */
 // elfinder.en.js is integrated into elfinder.(full|min).js by jake build
 if (typeof elFinder === 'function' && elFinder.prototype.i18) {
@@ -454,7 +454,8 @@ if (typeof elFinder === 'function' && elFinder.prototype.i18) {
 			'workspace'       : 'Work Space', // from v2.1.38 added 4.4.2018
 			'dialog'          : 'Dialog', // from v2.1.38 added 4.4.2018
 			'all'             : 'All', // from v2.1.38 added 4.4.2018
-			'iconSize'        : 'Icon Size (Icons view)', // form v2.1.39 added 7.5.2018
+			'iconSize'        : 'Icon Size (Icons view)', // from v2.1.39 added 7.5.2018
+			'editorMaximized' : 'Open the maximized editor window', // from v2.1.40 added 30.6.2018
 
 			/********************************** mimetypes **********************************/
 			'kindUnknown'     : 'Unknown',
