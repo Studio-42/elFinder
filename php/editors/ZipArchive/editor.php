99cbb0ffb586b1f3ed7e3e30abc232189d6c3a04
nao-pon <hypweb@gmail.com>
2018-07-09 11:58:03 +0900

    [php:editors] strict checking of constants

--- php/editors/ZipArchive/editor.php
+++ php/editors/ZipArchive/editor.php
@@ -4,7 +4,7 @@ class elFinderEditorZipArchive extends elFinderEditor
 {
     public function enabled()
     {
-        return !ELFINDER_DISABLE_ZIPEDITOR &&
+        return (!defined('ELFINDER_DISABLE_ZIPEDITOR') || !ELFINDER_DISABLE_ZIPEDITOR) &&
                class_exists('Barryvdh\elFinderFlysystemDriver\Driver') && 
                class_exists('League\Flysystem\Filesystem') &&
                class_exists('League\Flysystem\ZipArchive\ZipArchiveAdapter');
