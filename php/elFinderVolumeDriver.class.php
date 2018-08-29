38a21ce5a1ff08ee2ff2638e9fe36f38aa9a65ac
nao-pon <hypweb@gmail.com>
2018-08-29 17:37:28 +0900

    src build elFinder-2.1-4ef5cad

--- php/elFinderVolumeDriver.class.php
+++ php/elFinderVolumeDriver.class.php
@@ -320,7 +320,7 @@ abstract class elFinderVolumeDriver {
 		// MIME regex of send HTTP header "Content-Disposition: inline" or allow preview in quicklook
 		// '.' is allow inline of all of MIME types
 		// '$^' is not allow inline of all of MIME types
-		'dispInlineRegex' => '^(?:(?:image|video|audio)|application/(?:ogg|x-mpegURL|dash\+xml)|(?:text/plain|application/pdf)$)',
+		'dispInlineRegex' => '^(?:(?:video|audio)|image/(?!.+\+xml)|application/(?:ogg|x-mpegURL|dash\+xml)|(?:text/plain|application/pdf)$)',
 		// temporary content URL's base path
 		'tmpLinkPath'     => '',
 		// temporary content URL's base URL
