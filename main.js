c2ae871f6e75345cf64b5325d020e36bab1207f9
Naoki Sawada <hypweb@gmail.com>
2018-10-19 23:43:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -33,6 +33,14 @@
 			sortType : 'date',
 			sortOrder : 'desc',
 			sortStickFolders : false,
+			themes : {
+				'moono': 'https://nao-pon.github.io/elfinder-theme-manifests/moono.json',
+				'win10': 'https://nao-pon.github.io/elfinder-theme-manifests/win10.json',
+				'material': 'https://nao-pon.github.io/elfinder-theme-manifests/material-default.json',
+				'material-gray': 'https://nao-pon.github.io/elfinder-theme-manifests/material-gray.json',
+				'material-light': 'https://nao-pon.github.io/elfinder-theme-manifests/material-light.json',
+				'bootstrap': 'https://nao-pon.github.io/elfinder-theme-manifests/bootstrap.json'
+			},
 			ui : ['toolbar', 'places', 'tree', 'path', 'stat'],
 			commandsOptions : {
 				edit : {
