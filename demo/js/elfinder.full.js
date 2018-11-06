f44863b8cf391916c7bc371e227ed316190c1a18
nao-pon <hypweb@gmail.com>
2018-11-06 20:44:19 +0900

    update demo elFinder-2.1-dd205b7

--- demo/js/elfinder.full.js
+++ demo/js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1.42 (2.1-src Nightly: 909b7e9) (2018-11-05)
+ * Version 2.1.42 (2.1-src Nightly: dd205b7) (2018-11-06)
  * http://elfinder.org
  * 
  * Copyright 2009-2018, Studio 42
@@ -9949,7 +9949,7 @@ if (!window.cancelAnimationFrame) {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1.42 (2.1-src Nightly: 909b7e9)';
+elFinder.prototype.version = '2.1.42 (2.1-src Nightly: dd205b7)';
 
 
 
@@ -15769,8 +15769,7 @@ $.fn.elfindercwd = function(fm, options) {
 						.css('height', 'auto');
 					bottomMarker.hide();
 
-					wrapper[list ? 'addClass' : 'removeClass']('elfinder-cwd-wrapper-list')
-						._padding = parseInt(wrapper.css('padding-top')) + parseInt(wrapper.css('padding-bottom'));
+					wrapper[list ? 'addClass' : 'removeClass']('elfinder-cwd-wrapper-list');
 					if (fm.UA.iOS) {
 						wrapper.removeClass('overflow-scrolling-touch').addClass('overflow-scrolling-touch');
 					}
@@ -16428,11 +16427,13 @@ $.fn.elfindercwd = function(fm, options) {
 			restm = null,
 			resize = function(init) {
 				var initHeight = function() {
-					var h = 0;
-					wrapper.siblings('div.elfinder-panel:visible').each(function() {
-						h += $(this).outerHeight(true);
-					});
-					wrapper.height(wz.height() - h - wrapper._padding);
+					if (bufferExt.renderd) {
+						var h = 0;
+						wrapper.siblings('div.elfinder-panel:visible').each(function() {
+							h += $(this).outerHeight(true);
+						});
+						wrapper.height(wz.height() - h);
+					}
 				};
 				
 				init && initHeight();
