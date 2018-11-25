67f509a183e6b9be990fb1ba22d43181e3e42e3a
Naoki Sawada <hypweb@gmail.com>
2018-11-25 20:45:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -34,12 +34,13 @@
 			sortOrder : 'desc',
 			sortStickFolders : false,
 			themes : {
-				'moono': 'https://nao-pon.github.io/elfinder-theme-manifests/moono.json',
-				'win10': 'https://nao-pon.github.io/elfinder-theme-manifests/win10.json',
-				'material': 'https://nao-pon.github.io/elfinder-theme-manifests/material-default.json',
-				'material-gray': 'https://nao-pon.github.io/elfinder-theme-manifests/material-gray.json',
+				'dark-slim'     : 'https://nao-pon.github.io/elfinder-theme-manifests/dark-slim.json',
+				'material'      : 'https://nao-pon.github.io/elfinder-theme-manifests/material-default.json',
+				'material-gray' : 'https://nao-pon.github.io/elfinder-theme-manifests/material-gray.json',
 				'material-light': 'https://nao-pon.github.io/elfinder-theme-manifests/material-light.json',
-				'bootstrap': 'https://nao-pon.github.io/elfinder-theme-manifests/bootstrap.json'
+				'bootstrap'     : 'https://nao-pon.github.io/elfinder-theme-manifests/bootstrap.json',
+				'moono'         : 'https://nao-pon.github.io/elfinder-theme-manifests/moono.json',
+				'win10'         : 'https://nao-pon.github.io/elfinder-theme-manifests/win10.json'
 			},
 			ui : ['toolbar', 'places', 'tree', 'path', 'stat'],
 			commandsOptions : {
