e132e148e634bfcad3cc1d668923a8ae7ece04f6
nao-pon <hypweb@gmail.com>
2018-12-08 02:02:14 +0900

    src build elFinder-2.1-6d81de9

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1.43 (2.1-src Nightly: 5802535) (2018-12-07)
+ * Version 2.1.43 (2.1-src Nightly: 6d81de9) (2018-12-08)
  * http://elfinder.org
  * 
  * Copyright 2009-2018, Studio 42
@@ -9956,7 +9956,7 @@ if (!window.cancelAnimationFrame) {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1.43 (2.1-src Nightly: 5802535)';
+elFinder.prototype.version = '2.1.43 (2.1-src Nightly: 6d81de9)';
 
 
 
