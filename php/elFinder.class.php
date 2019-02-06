a19c2324ba7c155e12356a1608e9f6d50a7fc95e
nao-pon <hypweb@gmail.com>
2019-02-06 09:13:50 +0900

    [php:core] refactoring - row sort

--- php/elFinder.class.php
+++ php/elFinder.class.php
@@ -246,36 +246,36 @@ class elFinder
      * @var array
      **/
     protected $commands = array(
-        'open' => array('target' => false, 'tree' => false, 'init' => false, 'mimes' => false, 'compare' => false),
-        'ls' => array('target' => true, 'mimes' => false, 'intersect' => false),
-        'tree' => array('target' => true),
-        'parents' => array('target' => true, 'until' => false),
-        'tmb' => array('targets' => true),
-        'file' => array('target' => true, 'download' => false, 'cpath' => false, 'onetime' => false),
-        'zipdl' => array('targets' => true, 'download' => false),
-        'size' => array('targets' => true),
-        'mkdir' => array('target' => true, 'name' => false, 'dirs' => false),
-        'mkfile' => array('target' => true, 'name' => true, 'mimes' => false),
-        'rm' => array('targets' => true),
-        'rename' => array('target' => true, 'name' => true, 'mimes' => false, 'targets' => false, 'q' => false),
-        'duplicate' => array('targets' => true, 'suffix' => false),
-        'paste' => array('dst' => true, 'targets' => true, 'cut' => false, 'mimes' => false, 'renames' => false, 'hashes' => false, 'suffix' => false),
-        'upload' => array('target' => true, 'FILES' => true, 'mimes' => false, 'html' => false, 'upload' => false, 'name' => false, 'upload_path' => false, 'chunk' => false, 'cid' => false, 'node' => false, 'renames' => false, 'hashes' => false, 'suffix' => false, 'mtime' => false, 'overwrite' => false, 'contentSaveId' => false),
-        'get' => array('target' => true, 'conv' => false),
-        'put' => array('target' => true, 'content' => '', 'mimes' => false, 'encoding' => false),
+        'abort' => array('id' => true),
         'archive' => array('targets' => true, 'type' => true, 'mimes' => false, 'name' => false),
-        'extract' => array('target' => true, 'mimes' => false, 'makedir' => false),
-        'search' => array('q' => true, 'mimes' => false, 'target' => false, 'type' => false),
-        'info' => array('targets' => true, 'compare' => false),
-        'dim' => array('target' => true, 'substitute' => false),
-        'resize' => array('target' => true, 'width' => false, 'height' => false, 'mode' => false, 'x' => false, 'y' => false, 'degree' => false, 'quality' => false, 'bg' => false),
-        'netmount' => array('protocol' => true, 'host' => true, 'path' => false, 'port' => false, 'user' => false, 'pass' => false, 'alias' => false, 'options' => false),
-        'url' => array('target' => true, 'options' => false),
         'callback' => array('node' => true, 'json' => false, 'bind' => false, 'done' => false),
         'chmod' => array('targets' => true, 'mode' => true),
+        'dim' => array('target' => true, 'substitute' => false),
+        'duplicate' => array('targets' => true, 'suffix' => false),
+        'editor' => array('name' => true, 'method' => true, 'args' => false),
+        'extract' => array('target' => true, 'mimes' => false, 'makedir' => false),
+        'file' => array('target' => true, 'download' => false, 'cpath' => false, 'onetime' => false),
+        'get' => array('target' => true, 'conv' => false),
+        'info' => array('targets' => true, 'compare' => false),
+        'ls' => array('target' => true, 'mimes' => false, 'intersect' => false),
+        'mkdir' => array('target' => true, 'name' => false, 'dirs' => false),
+        'mkfile' => array('target' => true, 'name' => true, 'mimes' => false),
+        'netmount' => array('protocol' => true, 'host' => true, 'path' => false, 'port' => false, 'user' => false, 'pass' => false, 'alias' => false, 'options' => false),
+        'open' => array('target' => false, 'tree' => false, 'init' => false, 'mimes' => false, 'compare' => false),
+        'parents' => array('target' => true, 'until' => false),
+        'paste' => array('dst' => true, 'targets' => true, 'cut' => false, 'mimes' => false, 'renames' => false, 'hashes' => false, 'suffix' => false),
+        'put' => array('target' => true, 'content' => '', 'mimes' => false, 'encoding' => false),
+        'rename' => array('target' => true, 'name' => true, 'mimes' => false, 'targets' => false, 'q' => false),
+        'resize' => array('target' => true, 'width' => false, 'height' => false, 'mode' => false, 'x' => false, 'y' => false, 'degree' => false, 'quality' => false, 'bg' => false),
+        'rm' => array('targets' => true),
+        'search' => array('q' => true, 'mimes' => false, 'target' => false, 'type' => false),
+        'size' => array('targets' => true),
         'subdirs' => array('targets' => true),
-        'abort' => array('id' => true),
-        'editor' => array('name' => true, 'method' => true, 'args' => false)
+        'tmb' => array('targets' => true),
+        'tree' => array('target' => true),
+        'upload' => array('target' => true, 'FILES' => true, 'mimes' => false, 'html' => false, 'upload' => false, 'name' => false, 'upload_path' => false, 'chunk' => false, 'cid' => false, 'node' => false, 'renames' => false, 'hashes' => false, 'suffix' => false, 'mtime' => false, 'overwrite' => false, 'contentSaveId' => false),
+        'url' => array('target' => true, 'options' => false),
+        'zipdl' => array('targets' => true, 'download' => false)
     );
 
     /**
@@ -427,71 +427,71 @@ class elFinder
     protected $throwErrorOnExec = false;
 
     // Errors messages
-    const ERROR_UNKNOWN = 'errUnknown';
-    const ERROR_UNKNOWN_CMD = 'errUnknownCmd';
+    const ERROR_ACCESS_DENIED = 'errAccess';
+    const ERROR_ARC_MAXSIZE = 'errArcMaxSize';
+    const ERROR_ARC_SYMLINKS = 'errArcSymlinks';
+    const ERROR_ARCHIVE = 'errArchive';
+    const ERROR_ARCHIVE_EXEC = 'errArchiveExec';
+    const ERROR_ARCHIVE_TYPE = 'errArcType';
     const ERROR_CONF = 'errConf';
     const ERROR_CONF_NO_JSON = 'errJSON';
     const ERROR_CONF_NO_VOL = 'errNoVolumes';
-    const ERROR_INV_PARAMS = 'errCmdParams';
-    const ERROR_OPEN = 'errOpen';
+    const ERROR_CONV_UTF8 = 'errConvUTF8';
+    const ERROR_COPY = 'errCopy';
+    const ERROR_COPY_FROM = 'errCopyFrom';
+    const ERROR_COPY_ITSELF = 'errCopyInItself';
+    const ERROR_COPY_TO = 'errCopyTo';
+    const ERROR_CREATING_TEMP_DIR = 'errCreatingTempDir';
     const ERROR_DIR_NOT_FOUND = 'errFolderNotFound';
-    const ERROR_FILE_NOT_FOUND = 'errFileNotFound';     // 'File not found.'
-    const ERROR_TRGDIR_NOT_FOUND = 'errTrgFolderNotFound'; // 'Target folder "$1" not found.'
-    const ERROR_NOT_DIR = 'errNotFolder';
-    const ERROR_NOT_FILE = 'errNotFile';
-    const ERROR_PERM_DENIED = 'errPerm';
-    const ERROR_LOCKED = 'errLocked';        // '"$1" is locked and can not be renamed, moved or removed.'
     const ERROR_EXISTS = 'errExists';        // 'File named "$1" already exists.'
-    const ERROR_INVALID_NAME = 'errInvName';       // 'Invalid file name.'
+    const ERROR_EXTRACT = 'errExtract';
+    const ERROR_EXTRACT_EXEC = 'errExtractExec';
+    const ERROR_FILE_NOT_FOUND = 'errFileNotFound';     // 'File not found.'
+    const ERROR_FTP_DOWNLOAD_FILE = 'errFtpDownloadFile';
+    const ERROR_FTP_MKDIR = 'errFtpMkdir';
+    const ERROR_FTP_UPLOAD_FILE = 'errFtpUploadFile';
+    const ERROR_INV_PARAMS = 'errCmdParams';
     const ERROR_INVALID_DIRNAME = 'errInvDirname';    // 'Invalid folder name.'
+    const ERROR_INVALID_NAME = 'errInvName';       // 'Invalid file name.'
+    const ERROR_LOCKED = 'errLocked';        // '"$1" is locked and can not be renamed, moved or removed.'
+    const ERROR_MAX_TARGTES = 'errMaxTargets'; // 'Max number of selectable items is $1.'
     const ERROR_MKDIR = 'errMkdir';
     const ERROR_MKFILE = 'errMkfile';
-    const ERROR_RENAME = 'errRename';
-    const ERROR_COPY = 'errCopy';
-    const ERROR_MOVE = 'errMove';
-    const ERROR_COPY_FROM = 'errCopyFrom';
-    const ERROR_COPY_TO = 'errCopyTo';
-    const ERROR_COPY_ITSELF = 'errCopyInItself';
-    const ERROR_REPLACE = 'errReplace';          // 'Unable to replace "$1".'
-    const ERROR_RM = 'errRm';               // 'Unable to remove "$1".'
-    const ERROR_RM_SRC = 'errRmSrc';            // 'Unable remove source file(s)'
     const ERROR_MKOUTLINK = 'errMkOutLink';        // 'Unable to create a link to outside the volume root.'
-    const ERROR_UPLOAD = 'errUpload';           // 'Upload error.'
-    const ERROR_UPLOAD_FILE = 'errUploadFile';       // 'Unable to upload "$1".'
-    const ERROR_UPLOAD_NO_FILES = 'errUploadNoFiles';    // 'No files found for upload.'
-    const ERROR_UPLOAD_TOTAL_SIZE = 'errUploadTotalSize';  // 'Data exceeds the maximum allowed size.'
-    const ERROR_UPLOAD_FILE_SIZE = 'errUploadFileSize';   // 'File exceeds maximum allowed size.'
-    const ERROR_UPLOAD_FILE_MIME = 'errUploadMime';       // 'File type not allowed.'
-    const ERROR_UPLOAD_TRANSFER = 'errUploadTransfer';   // '"$1" transfer error.'
-    const ERROR_UPLOAD_TEMP = 'errUploadTemp';       // 'Unable to make temporary file for upload.'
-    const ERROR_ACCESS_DENIED = 'errAccess';
-    const ERROR_NOT_REPLACE = 'errNotReplace';       // Object "$1" already exists at this location and can not be replaced with object of another type.
-    const ERROR_SAVE = 'errSave';
-    const ERROR_EXTRACT = 'errExtract';
-    const ERROR_ARCHIVE = 'errArchive';
+    const ERROR_MOVE = 'errMove';
+    const ERROR_NETMOUNT = 'errNetMount';
+    const ERROR_NETMOUNT_FAILED = 'errNetMountFailed';
+    const ERROR_NETMOUNT_NO_DRIVER = 'errNetMountNoDriver';
+    const ERROR_NETUNMOUNT = 'errNetUnMount';
     const ERROR_NOT_ARCHIVE = 'errNoArchive';
-    const ERROR_ARCHIVE_TYPE = 'errArcType';
-    const ERROR_ARC_SYMLINKS = 'errArcSymlinks';
-    const ERROR_ARC_MAXSIZE = 'errArcMaxSize';
+    const ERROR_NOT_DIR = 'errNotFolder';
+    const ERROR_NOT_FILE = 'errNotFile';
+    const ERROR_NOT_REPLACE = 'errNotReplace';       // Object "$1" already exists at this location and can not be replaced with object of another type.
+    const ERROR_NOT_UTF8_CONTENT = 'errNotUTF8Content';
+    const ERROR_OPEN = 'errOpen';
+    const ERROR_PERM_DENIED = 'errPerm';
+    const ERROR_REAUTH_REQUIRE = 'errReauthRequire';  // 'Re-authorization is required.'
+    const ERROR_RENAME = 'errRename';
+    const ERROR_REPLACE = 'errReplace';          // 'Unable to replace "$1".'
     const ERROR_RESIZE = 'errResize';
     const ERROR_RESIZESIZE = 'errResizeSize';
-    const ERROR_UNSUPPORT_TYPE = 'errUsupportType';
-    const ERROR_CONV_UTF8 = 'errConvUTF8';
-    const ERROR_NOT_UTF8_CONTENT = 'errNotUTF8Content';
-    const ERROR_NETMOUNT = 'errNetMount';
-    const ERROR_NETUNMOUNT = 'errNetUnMount';
-    const ERROR_NETMOUNT_NO_DRIVER = 'errNetMountNoDriver';
-    const ERROR_NETMOUNT_FAILED = 'errNetMountFailed';
-    const ERROR_SESSION_EXPIRES = 'errSessionExpires';
-    const ERROR_CREATING_TEMP_DIR = 'errCreatingTempDir';
-    const ERROR_FTP_DOWNLOAD_FILE = 'errFtpDownloadFile';
-    const ERROR_FTP_UPLOAD_FILE = 'errFtpUploadFile';
-    const ERROR_FTP_MKDIR = 'errFtpMkdir';
-    const ERROR_ARCHIVE_EXEC = 'errArchiveExec';
-    const ERROR_EXTRACT_EXEC = 'errExtractExec';
+    const ERROR_RM = 'errRm';               // 'Unable to remove "$1".'
+    const ERROR_RM_SRC = 'errRmSrc';            // 'Unable remove source file(s)'
+    const ERROR_SAVE = 'errSave';
     const ERROR_SEARCH_TIMEOUT = 'errSearchTimeout';    // 'Timed out while searching "$1". Search result is partial.'
-    const ERROR_REAUTH_REQUIRE = 'errReauthRequire';  // 'Re-authorization is required.'
-    const ERROR_MAX_TARGTES = 'errMaxTargets'; // 'Max number of selectable items is $1.'
+    const ERROR_SESSION_EXPIRES = 'errSessionExpires';
+    const ERROR_TRGDIR_NOT_FOUND = 'errTrgFolderNotFound'; // 'Target folder "$1" not found.'
+    const ERROR_UNKNOWN = 'errUnknown';
+    const ERROR_UNKNOWN_CMD = 'errUnknownCmd';
+    const ERROR_UNSUPPORT_TYPE = 'errUsupportType';
+    const ERROR_UPLOAD = 'errUpload';           // 'Upload error.'
+    const ERROR_UPLOAD_FILE = 'errUploadFile';       // 'Unable to upload "$1".'
+    const ERROR_UPLOAD_FILE_MIME = 'errUploadMime';       // 'File type not allowed.'
+    const ERROR_UPLOAD_FILE_SIZE = 'errUploadFileSize';   // 'File exceeds maximum allowed size.'
+    const ERROR_UPLOAD_NO_FILES = 'errUploadNoFiles';    // 'No files found for upload.'
+    const ERROR_UPLOAD_TEMP = 'errUploadTemp';       // 'Unable to make temporary file for upload.'
+    const ERROR_UPLOAD_TOTAL_SIZE = 'errUploadTotalSize';  // 'Data exceeds the maximum allowed size.'
+    const ERROR_UPLOAD_TRANSFER = 'errUploadTransfer';   // '"$1" transfer error.'
 
     /**
      * Constructor
