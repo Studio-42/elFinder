4bc79c86b84e9c68b99586a2883d39f10a60a59c
nao-pon <hypweb@gmail.com>
2019-02-25 09:08:10 +0900

    update demo elFinder-2.1-a36a54b

--- demo/js/elfinder.full.js
+++ demo/js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1.46 (2.1-src Nightly: a78c6f4) (2019-02-25)
+ * Version 2.1.47 (2.1-src Nightly: a36a54b) (2019-02-25)
  * http://elfinder.org
  * 
  * Copyright 2009-2019, Studio 42
@@ -10136,7 +10136,7 @@ if (!window.cancelAnimationFrame) {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1.46 (2.1-src Nightly: a78c6f4)';
+elFinder.prototype.version = '2.1.47 (2.1-src Nightly: a36a54b)';
 
 
 
