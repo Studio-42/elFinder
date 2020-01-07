794d044d21ac1673cd8d86a4d6f0bf8188ffcde1
nao-pon <hypweb@gmail.com>
2020-01-07 15:56:34 +0900

    [editors:online-convert] online-convert.com can no longer be displayed in iFrames on external sites

--- js/extras/editors.default.js
+++ js/extras/editors.default.js
@@ -2647,7 +2647,7 @@
 				}
 			},
 			mimes : ['*'],
-			html : '<iframe style="width:100%;max-height:100%;border:none;"></iframe>',
+			html : '<div style="width:100%;max-height:100%;"></div>',
 			// setup on elFinder bootup
 			setup : function(opts, fm) {
 				var mOpts = opts.extraOptions.onlineConvert || {maxSize:100,showLink:true};
@@ -2751,8 +2751,6 @@
 										convert: con,
 										options: opts
 									});
-								} else {
-									open(cat, con);
 								}
 							}).on('change', function(e) {
 								var t = $(e.target),
@@ -2882,25 +2880,15 @@
 						}
 						return btns;
 					})(),
-					ifm = $(this).hide(),
-					select = $('<div/>')
+					select = $(this)
 						.append(
 							btns,
-							$('<div class="elfinder-edit-onlineconvert-bottom-btn"/>').append(
-								$('<button/>')
-									.addClass(fm.UA.iOS? 'elfinder-button-ios-multiline' : '')
-									.html(fm.i18n('convertOn', 'Online-Convert.com'))
-									.on('click', function() {
-										open();
-									})
-							),
 							(set.showLink? $(set.link) : null)
-						)
-						.appendTo(ifm.parent().css({overflow: 'auto'})),
+						),
 					spnr = $('<div class="elfinder-edit-spinner elfinder-edit-onlineconvert"/>')
 						.hide()
 						.html('<span class="elfinder-spinner-text">' + fm.i18n('nowLoading') + '</span><span class="elfinder-spinner"/>')
-						.appendTo(ifm.parent()),
+						.appendTo(select.parent()),
 					_url = null,
 					url = function() {
 						var onetime;
@@ -2978,13 +2966,14 @@
 							}
 							fm.toast({
 								msg: fm.i18n('editorConvNoApi'),
-								mode: 'warning',
+								mode: 'error',
 								timeOut: 3000,
 								onHidden: function() {
 									uiToast.children().length === 1 && uiToast.appendTo(fm.getUI());
-									open(cat, con);
 								}
 							});
+							spnr.hide();
+							select.show();
 						}
 					},
 					setStatus = function(status) {
@@ -3028,61 +3017,9 @@
 							});
 						}
 					},
-					open = function(cat, con) {
-						var link;
-						if (cat && con) {
-							if (set.conv[cat] && set.conv[cat][con] && set.conv[cat][con].link) {
-								link = set.conv[cat][con].link.replace('%s', con);
-							} else {
-								link = cat === 'hash'? ('/' + con + '-generator') : ('/convert-to-' + con);
-							}
-							link = set.url.replace('%s', cat).replace('%s', link);
-						} else {
-							link = set.url.replace('%s', mode + '-conversion').replace('%s', '');
-						}
-						spnr.hide();
-						select.hide();
-						ifm.parent().css({overflow: fm.UA.iOS? 'auto' : 'hidden'});
-						$(ta).data('dfrd', url().done(function(url) {
-							var opts;
-							if (url) {
-								opts = {
-									css: {
-										height: '100%'
-									}
-								};
-								// trigger event 'editEditorPrepare'
-								ta.editor.trigger('Prepare', {
-									node: ta,
-									editorObj: void(0),
-									instance: ifm,
-									opts: opts
-								});
-								url = link + encodeURIComponent(fm.convAbsUrl(url));
-								ifm.attr('src', url).show().css(opts.css)
-									.one('load', function() {
-										uiToast.appendTo(ta.closest('.ui-dialog'));
-										fm.toast({
-											msg: fm.i18n('editorConvNeedUpload'),
-											mode: 'info',
-											timeOut: 15000,
-											onHidden: function() {
-												uiToast.children().length === 1 && uiToast.appendTo(fm.getUI());
-											},
-											button: {
-												text: 'btnYes'
-											}
-										});
-									});
-							} else {
-								data.error && fm.error(data.error);
-								ta.elfinderdialog('destroy');
-							}
-						}));
-					},
 					mode = 'document',
 					cl, m;
-				ifm.parent().addClass('overflow-scrolling-touch');
+				select.parent().css({overflow: 'auto'}).addClass('overflow-scrolling-touch');
 				if (m = file.mime.match(/^(audio|image|video)/)) {
 					mode = m[1];
 				}
@@ -3108,14 +3045,12 @@
 							setOptions(t);
 						});
 					}
-					ifm.parent().scrollTop(btns.children('.onlineconvert-fieldset-' + mode).offset().top);
+					select.parent().scrollTop(btns.children('.onlineconvert-fieldset-' + mode).offset().top);
 				}
 			},
 			load : function() {},
 			getContent : function() {},
 			save : function() {},
-			// Before dialog close
-			beforeclose : iframeClose,
 			// On dialog closed
 			close : function(ta) {
 				var fm = this.fm,
