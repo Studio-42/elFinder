ba75974f83627553aef25a6e6b14c58d590012cd
nao-pon <hypweb@gmail.com>
2020-01-15 17:09:36 +0900

    [js] add worker/worker.js to support the Window.Worker

---
+++ js/worker/worker.js
@@ -0,0 +1,14 @@
+self.onmessage = e => {
+	const d = e.data;
+	try {
+		self.data = d.data;
+		if (d.scripts) {
+			for(let i = 0; i < d.scripts.length; i++) {
+				importScripts(d.scripts[i]);
+			}
+		}
+		self.postMessage(self.res);
+	} catch (e) {
+		self.postMessage({error: e.toString()});
+	}
+};
