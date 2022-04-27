84f07fd18a4fc91d769aef35bb26da9addf851c3
nao-pon <hypweb@gmail.com>
2022-04-27 23:14:16 +0900

    [js:options] update CDNs

--- js/extras/editors.default.js
+++ js/extras/editors.default.js
@@ -218,7 +218,7 @@
 					this.disabled = true;
 				} else {
 					this.opts = Object.assign({
-						version: 'v3.15.2'
+						version: 'v3.15.3'
 					}, opts.extraOptions.tuiImgEditOpts || {}, {
 						iconsPath : fm.baseUrl + 'img/tui-',
 						theme : {}
