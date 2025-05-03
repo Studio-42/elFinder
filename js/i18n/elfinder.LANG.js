50c4406bdd86facb681b10c6756e40a937ccb6ce
jmario74 <84346469+jmario74@users.noreply.github.com>
2025-05-03 07:23:54 -0700

    Update elfinder.LANG.js
    added file type message "Webp image" to column type "kind" for webp file when in list view

--- js/i18n/elfinder.LANG.js
+++ js/i18n/elfinder.LANG.js
@@ -563,6 +563,7 @@
 			'kindPSD'         : 'Adobe Photoshop image',
 			'kindXBITMAP'     : 'X bitmap image',
 			'kindPXM'         : 'Pixelmator image',
+			'kindWEBP'	  : 'WEBP image',
 			// media
 			'kindAudio'       : 'Audio media',
 			'kindAudioMPEG'   : 'MPEG audio',
