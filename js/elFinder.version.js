e311a7db22adb344997ddd8d070fff745ea973e7
nao-pon <1412630+nao-pon@users.noreply.github.com>
2026-05-07 21:52:56 +0900

    elFinder version 2.1.69

--- js/elFinder.version.js
+++ js/elFinder.version.js
@@ -3,5 +3,5 @@
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1.68';
+elFinder.prototype.version = '2.1.69';
 
--- php/elFinder.class.php
+++ php/elFinder.class.php
@@ -32,7 +32,7 @@ class elFinder
      *
      * @var integer
      */
-    protected static $ApiRevision = 68;
+    protected static $ApiRevision = 69;
 
     /**
      * Storages (root dirs)
